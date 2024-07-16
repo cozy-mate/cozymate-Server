5b2fb347180b90a0f3fec4129cb766b09440ea48
Ga Dong Sik <ret0422@gmail.com>
2024-07-16 10:15:14 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/test/java/com/cozymate/cozymate_server/CozymateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cozymate.cozymate_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozymateServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
