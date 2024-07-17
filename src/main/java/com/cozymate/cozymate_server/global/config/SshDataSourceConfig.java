1d3e0f5f04e60bc2b7fd96b9c02b224289c9c60c
Ga Dong Sik <ret0422@gmail.com>
2024-07-17 15:40:04 +0900

    feat: SSH 터널링 -> MySQL 연결 기능 구현

---
+++ src/main/java/com/cozymate/cozymate_server/global/config/SshDataSourceConfig.java
@@ -0,0 +1,36 @@
+package com.cozymate.cozymate_server.global.config;
+
+
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class SshDataSourceConfig {
+
+    private final SshTunnelConfig initializer;
+
+    @Bean("dataSource")
+    @Primary
+    public DataSource dataSource(DataSourceProperties properties) {
+        log.info("Datasource Properties URL: {}", properties.getUrl());
+        log.info("Datasource Properties Username: {}", properties.getUsername());
+        log.info("Datasource Properties Driver ClassName: {}", properties.getDriverClassName());
+        Integer forwardedPort = initializer.buildSshConnection();
+        String url = properties.getUrl().replace("[forwardedPort]", String.valueOf(forwardedPort));
+        log.info(url);
+        return DataSourceBuilder.create()
+            .url(url)
+            .username(properties.getUsername())
+            .password(properties.getPassword())
+            .driverClassName(properties.getDriverClassName())
+            .build();
+    }
+}
