096e8c91bd00ba5a0b8b9c1a2047d86c8eab6f22
Ga Dong Sik <ret0422@gmail.com>
2024-07-18 00:37:08 +0900

    [COZY-6] feat: CI/CD 세팅

--- src/main/java/com/cozymate/cozymate_server/global/config/SshDataSourceConfig.java
+++ src/main/java/com/cozymate/cozymate_server/global/config/SshDataSourceConfig.java
@@ -4,6 +4,7 @@ package com.cozymate.cozymate_server.global.config;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,14 +18,27 @@ public class SshDataSourceConfig {
 
     private final SshTunnelConfig initializer;
 
+    @Value("${server}")
+    private String isServer;
+    @Value("${cloud.aws.ec2.database_endpoint}")
+    private String databaseEndpoint;
+
     @Bean("dataSource")
     @Primary
     public DataSource dataSource(DataSourceProperties properties) {
-        log.info("Datasource Properties URL: {}", properties.getUrl());
+        String url = properties.getUrl().replace("localhost", databaseEndpoint);
+        url = url.replace("[forwardedPort]", String.valueOf(3306));
+        Integer forwardedPort = null;
+        if (isServer.equals("false")) {
+            url = properties.getUrl(); // jdbc:mysql://localhost:[forwardedPort]/dev
+            forwardedPort = initializer.buildSshConnection();
+            url = url.replace("[forwardedPort]", String.valueOf(forwardedPort));
+        }
+
+        log.info("Datasource Properties URL: {}", url);
         log.info("Datasource Properties Username: {}", properties.getUsername());
         log.info("Datasource Properties Driver ClassName: {}", properties.getDriverClassName());
-        Integer forwardedPort = initializer.buildSshConnection();
-        String url = properties.getUrl().replace("[forwardedPort]", String.valueOf(forwardedPort));
+
         log.info(url);
         return DataSourceBuilder.create()
             .url(url)
