efb3a3403d67a2c17b68ed2e00cd244dfe7b010d
subin <118423039+suuu0719@users.noreply.github.com>
2024-07-28 15:02:26 +0900

    [COZY-77] 방 생성 API
    * feat: 방 생성 API
    
    * [COZY-77] feat: 방 생성 API
    
    * [COZY-77] refactor: 피드백 반영 및 파일 구조 변경
    
    * [COZY-77] refactor: ResponseEntity return 형식 통일
    
    ---------
    
    Co-authored-by: Ga Dong Sik <62535229+eple0329@users.noreply.github.com>

--- src/main/java/com/cozymate/cozymate_server/domain/mate/repository/MateRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/mate/repository/MateRepository.java
@@ -1,5 +1,6 @@
-package com.cozymate.cozymate_server.domain.mate;
+package com.cozymate.cozymate_server.domain.mate.repository;
 
+import com.cozymate.cozymate_server.domain.mate.Mate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MateRepository extends JpaRepository<Mate, Long> {
--- src/main/java/com/cozymate/cozymate_server/global/response/code/status/ErrorStatus.java
+++ src/main/java/com/cozymate/cozymate_server/global/response/code/status/ErrorStatus.java
@@ -19,12 +19,16 @@ public enum ErrorStatus implements BaseErrorCode {
     // Member 관련 에러
     _MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER400", "멤버를 찾을 수 없습니다."),
 
+    // Room
+    _ROOM_NOT_FOUND(HttpStatus.BAD_REQUEST, "ROOM400", "존재하지 않는 방입니다."),
+    _ROOM_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "ROOM409", "이미 활성화 또는 대기 중인 방이 존재합니다.")
+
     // University 관련 에러
     _UNIVERSITY_NOT_FOUND(HttpStatus.BAD_REQUEST,"UNIVERSITY400","대학을 찾을 수 없습니다."),
 
     // MemberStat 관련 에러
     _MEMBERSTAT_EXISTS(HttpStatus.BAD_REQUEST,"MEMBERSTAT400","멤버 상세정보가 이미 존재합니다."),
-    _MEMBERSTAT_MERIDIAN_NOT_VALID(HttpStatus.BAD_REQUEST,"MEMBERSTAT401","오전, 오후를 정확하게 입력하세요.")
+    _MEMBERSTAT_MERIDIAN_NOT_VALID(HttpStatus.BAD_REQUEST,"MEMBERSTAT401","오전, 오후를 정확하게 입력하세요."),
     ;
 
 
