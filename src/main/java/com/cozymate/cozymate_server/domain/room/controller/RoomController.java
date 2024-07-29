213d23342b35bf9e8a985af7775ed68aa8b43fc5
suuu0719 <suuu0719@gmail.com>
2024-07-30 01:23:48 +0900

    [COZY-79] feature: 생성한 방 정보 조회

--- src/main/java/com/cozymate/cozymate_server/domain/room/controller/RoomController.java
+++ src/main/java/com/cozymate/cozymate_server/domain/room/controller/RoomController.java
@@ -1,12 +1,17 @@
 package com.cozymate.cozymate_server.domain.room.controller;
 
-import com.cozymate.cozymate_server.domain.room.service.RoomCommandService;
+
 import com.cozymate.cozymate_server.domain.room.dto.RoomCreateRequest;
+import com.cozymate.cozymate_server.domain.room.dto.RoomCreateResponse;
+import com.cozymate.cozymate_server.domain.room.service.RoomCommandService;
 import com.cozymate.cozymate_server.global.response.ApiResponse;
+import com.cozymate.cozymate_server.global.response.code.status.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +30,14 @@ public class RoomController {
         // TODO: 시큐리티 이용해 사용자 인증 받아야 함.
         roomCommandService.createRoom(request);
         return ResponseEntity.ok(ApiResponse.onSuccess("방 생성 완료"));
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "[바니] 생성한 방 정보 조회 기능", description = "방 아이디를 입력합니다.")
+    public ResponseEntity<ApiResponse<RoomCreateResponse>> getRoom(@PathVariable Long id) {
+        RoomCreateResponse response = roomCommandService.getRoomById(id);
+        return ResponseEntity.status(SuccessStatus._OK.getHttpStatus())
+            .body(ApiResponse.onSuccess(response));
     }
 
 }
