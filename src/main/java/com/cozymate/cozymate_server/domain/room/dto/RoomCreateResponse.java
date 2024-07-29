3073c01e92d4b51c6900daf907410adafeef2536
suuu0719 <suuu0719@gmail.com>
2024-07-30 01:29:38 +0900

    [COZY-79] fix: RoomCreateResponse 어노테이션 수정

--- src/main/java/com/cozymate/cozymate_server/domain/room/dto/RoomCreateResponse.java
+++ src/main/java/com/cozymate/cozymate_server/domain/room/dto/RoomCreateResponse.java
@@ -1,20 +1,14 @@
 package com.cozymate.cozymate_server.domain.room.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class RoomCreateResponse {
 
     private String name;
     private String inviteCode;
     private Integer profileImage;
-
-    public RoomCreateResponse(String name, String inviteCode, Integer profileImage) {
-        this.name = name;
-        this.inviteCode = inviteCode;
-        this.profileImage = profileImage;
-    }
 
 }
