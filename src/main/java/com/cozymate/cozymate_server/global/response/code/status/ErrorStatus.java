fce1517c12f2d98d07dbaaabf76ed5c65a821582
veronees <151748893+veronees@users.noreply.github.com>
2024-07-31 21:56:16 +0900

    [COZY-49] feat: 쪽지방 삭제
    Co-authored-by: Ga Dong Sik <62535229+eple0329@users.noreply.github.com>

--- src/main/java/com/cozymate/cozymate_server/global/response/code/status/ErrorStatus.java
+++ src/main/java/com/cozymate/cozymate_server/global/response/code/status/ErrorStatus.java
@@ -35,6 +35,10 @@ public enum ErrorStatus implements BaseErrorCode {
     _MEMBERSTAT_EXISTS(HttpStatus.BAD_REQUEST,"MEMBERSTAT400","멤버 상세정보가 이미 존재합니다."),
     _MEMBERSTAT_MERIDIAN_NOT_VALID(HttpStatus.BAD_REQUEST,"MEMBERSTAT401","오전, 오후를 정확하게 입력하세요."),
 
+    // ChatRoom 관련 애러
+    _CHATROOM_NOT_FOUND(HttpStatus.BAD_REQUEST, "CHATROOM400", "쪽지방을 찾을 수 없습니다."),
+    _CHATROOM_FORBIDDEN(HttpStatus.BAD_REQUEST,"CHATROOM401", "해당 쪽지방을 삭제할 권한이 없습니다."),
+
     // Mate 관련
     _MATE_NOT_FOUND(HttpStatus.BAD_REQUEST, "MATE400", "해당하는 메이트 정보가 없습니다."),
 
