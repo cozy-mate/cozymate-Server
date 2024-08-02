99a77c6b3d41b1c33364100693b4710db0fe27ed
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-08-02 17:03:57 +0900

    [COZY-61] feat: 사용자 Member 엔티티 추가로 필요한 필드 추가

--- src/main/java/com/cozymate/cozymate_server/domain/member/Member.java
+++ src/main/java/com/cozymate/cozymate_server/domain/member/Member.java
@@ -1,47 +1,67 @@
 package com.cozymate.cozymate_server.domain.member;
 
+
 import com.cozymate.cozymate_server.domain.member.enums.Gender;
-import com.cozymate.cozymate_server.domain.university.University;
+import com.cozymate.cozymate_server.domain.member.enums.Role;
+import com.cozymate.cozymate_server.domain.member.enums.SocialType;
+
 import com.cozymate.cozymate_server.global.utils.BaseTimeEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
-import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
+import java.util.Collection;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private University university;
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
 
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @NonNull
     private String clientId;
 
+
+    @NonNull
     private String name;
 
+    @NonNull
     private String nickname;
 
+    @NonNull
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    @NonNull
     private LocalDate birthDay;
 
-    // 캐릭터 이름이 삭제된다는 기획상의 수정으로 프론트에서 정수로 받고,
-    // s3에서 /persona/1 과 같은 식으로 가져오는게 좋아보입니다.
+    @NonNull
     private Integer persona;
 }
