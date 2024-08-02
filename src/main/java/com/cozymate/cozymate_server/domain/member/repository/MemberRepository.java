b242f4cc22941b005397408240ec7b14b3b59bab
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-08-02 17:10:48 +0900

    [COZY-61] add: 닉네임 중복검증, clientId로 사용자 인스턴스 찾는 함수 추가

---
+++ src/main/java/com/cozymate/cozymate_server/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.cozymate.cozymate_server.domain.member.repository;
+
+import com.cozymate.cozymate_server.domain.member.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Boolean existsByNickname(String nickname);
+
+    Optional<Member> findByClientId(String clientId);
+}
