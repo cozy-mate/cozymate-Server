6ea4d938332508aab66e0c7e0fb2c116e98a5f48
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-08-02 17:09:20 +0900

    [COZY-61] feat: 로그인 관련 에러 코드

---
+++ src/main/java/com/cozymate/cozymate_server/domain/auth/AuthErrorStatus.java
@@ -0,0 +1,31 @@
+package com.cozymate.cozymate_server.domain.auth;
+
+import com.cozymate.cozymate_server.global.response.code.BaseErrorCode;
+import com.cozymate.cozymate_server.global.response.code.ErrorReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AuthErrorStatus implements BaseErrorCode {
+
+    _KAKAO_ACCESS_TOKEN_PARSING_FAIL(
+            HttpStatus.INTERNAL_SERVER_ERROR, "AUTH5001", "카카오 accessToken 파싱 실패"),
+
+    ;
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+
+    @Override
+    public ErrorReasonDto getReasonHttpStatus() {
+        return ErrorReasonDto.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .build();
+    }
+}
