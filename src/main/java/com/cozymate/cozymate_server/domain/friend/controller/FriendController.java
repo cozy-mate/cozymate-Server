ad9097ab64396e988cc730b83145db26b4255568
jpark0506 <peterp1@inha.edu>
2024-08-03 23:47:33 +0900

    [COZY-117] refactor: Friend Controller에 Error 관련 SwaggerAnnotation 추가

--- src/main/java/com/cozymate/cozymate_server/domain/friend/controller/FriendController.java
+++ src/main/java/com/cozymate/cozymate_server/domain/friend/controller/FriendController.java
@@ -4,7 +4,9 @@ import com.cozymate.cozymate_server.domain.friend.dto.FriendRequestDTO;
 import com.cozymate.cozymate_server.domain.friend.service.FriendCommandService;
 import com.cozymate.cozymate_server.domain.member.Member;
 import com.cozymate.cozymate_server.global.response.ApiResponse;
+import com.cozymate.cozymate_server.global.response.code.status.ErrorStatus;
 import com.cozymate.cozymate_server.global.response.code.status.SuccessStatus;
+import com.cozymate.cozymate_server.global.utils.SwaggerApiError;
 import com.sun.net.httpserver.Authenticator.Success;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -39,6 +41,11 @@ public class FriendController {
         summary = "[포비] 친구 신청 요청",
         description = "Path Variable로 쪽지를 보내는 멤버의 ID, Body로 쪽지를 받는 멤버의 ID를 보내주세요."
     )
+    @SwaggerApiError({
+        ErrorStatus._MEMBER_NOT_FOUND,
+        ErrorStatus._FRIEND_REQUEST_SENT,
+        ErrorStatus._FRIEND_REQUEST_RECEIVED
+    })
     @PostMapping("/request/{senderId}")
     public ResponseEntity<ApiResponse<Long>> createFriendRequest(
         @PathVariable Long senderId,
@@ -56,6 +63,11 @@ public class FriendController {
         summary = "[포비] 친구 신청 수락",
         description = "Path Variable로 쪽지를 받은 멤버의 ID, Body로 쪽지를 보냈던 멤버의 ID를 보내주세요."
     )
+    @SwaggerApiError({
+        ErrorStatus._MEMBER_NOT_FOUND,
+        ErrorStatus._FRIEND_REQUEST_NOT_FOUND,
+        ErrorStatus._FRIEND_REQUEST_RECEIVED
+    })
     @PutMapping("/accept/{accepterId}")
     public ResponseEntity<ApiResponse<Long>> acceptFriendRequest(
         @PathVariable Long accepterId,
@@ -73,6 +85,11 @@ public class FriendController {
         summary = "[포비] 친구 신청 거절",
         description = "Path Variable로 쪽지를 받은 멤버의 ID, Body로 쪽지를 보냈던 멤버의 ID를 보내주세요."
     )
+    @SwaggerApiError({
+        ErrorStatus._MEMBER_NOT_FOUND,
+        ErrorStatus._FRIEND_REQUEST_NOT_FOUND,
+        ErrorStatus._FRIEND_REQUEST_ACCEPTED
+    })
     @DeleteMapping("/deny/{accepterId}")
     public ResponseEntity<ApiResponse<Long>> denyFriendRequest(
         @PathVariable Long accepterId,
