9d77a8c9f7b7f396d31af06e855c77dd3ecfd64e
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-08-05 01:40:26 +0900

    [COZY-61] fix: SwaggerConfig에 인증 및 토큰 을 위한 헤더 추가

--- src/main/java/com/cozymate/cozymate_server/global/config/SwaggerConfig.java
+++ src/main/java/com/cozymate/cozymate_server/global/config/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package com.cozymate.cozymate_server.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,18 +15,35 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        // Define the security scheme
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Define the security requirement
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Authorization");
+
+        // Add the security scheme to components
+        Components components = new Components()
+                .addSecuritySchemes("Authorization", apiKey);
+
         return new OpenAPI()
-            .addServersItem(new Server().url("/"))
-            .info(apiInfo());
+                .addServersItem(new Server().url("/"))
+                .components(components)
+                .addSecurityItem(securityRequirement)
+                .info(apiInfo());
     }
 
     private Info apiInfo() {
         return new Info()
-            .title("Cozymate Spring Swagger")
-            .description("Cozymate Swagger Doc입니다.")
-            .version("0.0.1");
-            //.contact(new Contact().name("gmail").url("https://www.google.com/intl/ko/gmail/about/"))
-            //.description("잘못된 부분이나 오류 발생 시 해당 메일로 문의해주세요."); // TODO: 추가 예정
+                .title("Cozymate Spring Swagger")
+                .description("Cozymate Swagger Doc입니다.")
+                .version("0.0.1");
+        //.contact(new Contact().name("gmail").url("https://www.google.com/intl/ko/gmail/about/"))
+        //.description("잘못된 부분이나 오류 발생 시 해당 메일로 문의해주세요."); // TODO: 추가 예정
     }
-
 }
