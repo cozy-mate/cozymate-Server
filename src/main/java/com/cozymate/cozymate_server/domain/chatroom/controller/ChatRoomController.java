b02b30dc66ce624f05ad7b611d76ff1a94f644da
veronees <jsg6854@naver.com>
2024-08-04 19:49:04 +0900

    [COZY-47] fix: @SwaggerApiError 추가

--- src/main/java/com/cozymate/cozymate_server/domain/chatroom/controller/ChatRoomController.java
+++ src/main/java/com/cozymate/cozymate_server/domain/chatroom/controller/ChatRoomController.java
@@ -4,6 +4,8 @@ import com.cozymate.cozymate_server.domain.chatroom.dto.ChatRoomResponseDto;
 import com.cozymate.cozymate_server.domain.chatroom.service.ChatRoomCommandService;
 import com.cozymate.cozymate_server.domain.chatroom.service.ChatRoomQueryService;
 import com.cozymate.cozymate_server.global.response.ApiResponse;
+import com.cozymate.cozymate_server.global.response.code.status.ErrorStatus;
+import com.cozymate.cozymate_server.global.utils.SwaggerApiError;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -35,8 +37,10 @@ public class ChatRoomController {
     // TODO: memberId 추후 시큐리티 인증 객체에서 받아오는 것으로 변경 예정
     @GetMapping
     @Operation(summary = "[베로] 쪽지방 목록 조회", description = "파라미터에 자신의 memberId")
+    @SwaggerApiError({ErrorStatus._MEMBER_NOT_FOUND, ErrorStatus._CHAT_NOT_FOUND})
     public ResponseEntity<ApiResponse<List<ChatRoomResponseDto>>> getChatRoomList(
         @RequestParam Long memberId) {
-        return ResponseEntity.ok(ApiResponse.onSuccess(chatRoomQueryService.getChatRoomList(memberId)));
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(chatRoomQueryService.getChatRoomList(memberId)));
     }
 }
