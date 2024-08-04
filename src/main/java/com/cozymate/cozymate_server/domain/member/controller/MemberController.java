d46a67e1e39c43b63c6eac48f9e73b5628e19474
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-08-04 21:40:25 +0900

    [COZY-61] fix: MemberController 반환값 ResponseEntity로 수정

--- src/main/java/com/cozymate/cozymate_server/domain/member/controller/MemberController.java
+++ src/main/java/com/cozymate/cozymate_server/domain/member/controller/MemberController.java
@@ -5,9 +5,11 @@ import com.cozymate.cozymate_server.domain.member.dto.MemberRequestDTO;
 import com.cozymate.cozymate_server.domain.member.dto.MemberResponseDTO;
 import com.cozymate.cozymate_server.domain.member.service.MemberService;
 import com.cozymate.cozymate_server.global.response.ApiResponse;
+import com.cozymate.cozymate_server.global.response.code.status.SuccessStatus;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,35 +27,35 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/check-nickname")
-    ApiResponse<Boolean> checkNickname(@RequestParam String nickname) {
+    ResponseEntity<ApiResponse<Boolean>> checkNickname(@RequestParam String nickname) {
         Boolean isValid = memberService.checkNickname(nickname);
-        return ApiResponse.onSuccess(isValid);
+        return ResponseEntity.status(SuccessStatus._OK.getHttpStatus()).body(ApiResponse.onSuccess(isValid));
     }
 
     @PostMapping("/join")
-    ApiResponse<MemberResponseDTO.LoginResponseDTO> join(
+    ResponseEntity<ApiResponse<MemberResponseDTO.LoginResponseDTO>> join(
             @RequestAttribute("client_id") String clientId,
             @RequestBody @Valid MemberRequestDTO.JoinRequestDTO joinRequestDTO
     ) {
         // todo : 파라미터 바인딩 검증 로직 추가
         MemberResponseDTO.LoginResponseDTO loginResponseDTO = memberService.join(clientId, joinRequestDTO);
-        return ApiResponse.onSuccess(loginResponseDTO);
+        return ResponseEntity.status(SuccessStatus._OK.getHttpStatus()).body(ApiResponse.onSuccess(loginResponseDTO));
     }
 
     @GetMapping("/login")
-    ApiResponse<MemberResponseDTO.LoginResponseDTO> login(
+    ResponseEntity<ApiResponse<MemberResponseDTO.LoginResponseDTO>> login(
             @AuthenticationPrincipal MemberDetails memberDetails
     ) {
         MemberResponseDTO.LoginResponseDTO loginResponseDTO = memberService.login(memberDetails);
-        return ApiResponse.onSuccess(loginResponseDTO);
+        return ResponseEntity.status(SuccessStatus._OK.getHttpStatus()).body(ApiResponse.onSuccess(loginResponseDTO));
     }
 
     @GetMapping("/member-info")
-    ApiResponse<MemberResponseDTO.MemberInfoDTO> getMemberInfo(
+    ResponseEntity<ApiResponse<MemberResponseDTO.MemberInfoDTO>> getMemberInfo(
             @AuthenticationPrincipal MemberDetails memberDetails
     ) {
-        MemberResponseDTO.MemberInfoDTO memberInfo = memberService.getMemberInfo(memberDetails);
-        return ApiResponse.onSuccess(memberInfo);
+        MemberResponseDTO.MemberInfoDTO memberInfoDTO = memberService.getMemberInfo(memberDetails);
+        return ResponseEntity.status(SuccessStatus._OK.getHttpStatus()).body(ApiResponse.onSuccess(memberInfoDTO));
     }
 
 
