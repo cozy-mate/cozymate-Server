65cc899e9dd9844815b17c93ffd179ab976144e2
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-08-05 21:08:17 +0900

    [COZY-61] refactor: MemberRepository 에 대한 의존성 삭제 및 memberQueryService 에 대한 의존성 추가

--- src/main/java/com/cozymate/cozymate_server/domain/auth/service/AuthService.java
+++ src/main/java/com/cozymate/cozymate_server/domain/auth/service/AuthService.java
@@ -1,19 +1,15 @@
 package com.cozymate.cozymate_server.domain.auth.service;
 
-import static com.cozymate.cozymate_server.domain.auth.dto.AuthResponseDTO.RE_LOGIN_EXISTING_MEMBER_MESSAGE;
-import static com.cozymate.cozymate_server.domain.auth.dto.AuthResponseDTO.TEMPORARY_TOKEN_SUCCESS_MESSAGE;
-
 import com.cozymate.cozymate_server.domain.auth.Token;
 import com.cozymate.cozymate_server.domain.auth.dto.AuthResponseDTO;
 import com.cozymate.cozymate_server.domain.auth.repository.TokenRepository;
-import com.cozymate.cozymate_server.domain.auth.utils.MemberDetails;
-import com.cozymate.cozymate_server.domain.auth.utils.TemporaryMember;
-import com.cozymate.cozymate_server.domain.auth.utils.jwt.JwtUtil;
+import com.cozymate.cozymate_server.domain.auth.userDetails.MemberDetails;
+import com.cozymate.cozymate_server.domain.auth.userDetails.TemporaryMember;
+import com.cozymate.cozymate_server.domain.auth.utils.JwtUtil;
 
-import com.cozymate.cozymate_server.domain.member.Member;
-import com.cozymate.cozymate_server.domain.member.repository.MemberRepository;
-
-import java.util.Optional;
+import com.cozymate.cozymate_server.domain.member.service.MemberQueryService;
+import com.cozymate.cozymate_server.global.response.code.status.ErrorStatus;
+import com.cozymate.cozymate_server.global.response.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
@@ -29,27 +25,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AuthService implements UserDetailsService {
 
-    private final JwtUtil jwtUtil;
-    private final MemberRepository memberRepository;
-    private final TokenRepository tokenRepository;
+    public static final String TEMPORARY_TOKEN_SUCCESS_MESSAGE = "임시 토큰 발급 완료";
 
+    public static final String RE_LOGIN_EXISTING_MEMBER_MESSAGE = "기존 사용자 재로그인";
+
+    private final JwtUtil jwtUtil;
+    private final MemberQueryService memberQueryService;
+    private final TokenRepository tokenRepository;
 
 
     // 기존에 있는 회원이면 AccessToken, 임시회원이면 TempToken 발급
     @Transactional
     public String generateToken(String clientId) {
         UserDetails userDetails = loadUserByUsername(clientId);
-        Optional<Member> member = memberRepository.findByClientId(clientId);
-        if (member.isPresent()) {
+        // 이미 회원인 경우
+        if (memberQueryService.isPresent(clientId)) {
             String accessToken = jwtUtil.generateAccessToken(userDetails);
             String refreshToken = jwtUtil.generateRefreshToken(userDetails);
             Token newToken = new Token(clientId, refreshToken);
             tokenRepository.save(newToken);
-
             return accessToken;
-        } else {
-            return jwtUtil.generateTemporaryToken(userDetails);
         }
+        // 새로 가입한 경우
+        return jwtUtil.generateTemporaryToken(userDetails);
     }
 
     public HttpHeaders addTokenAtHeader(String token) {
@@ -59,41 +57,43 @@ public class AuthService implements UserDetailsService {
         return headers;
     }
 
-    @Transactional
     public AuthResponseDTO.SocialLoginDTO socialLogin(String clientId) {
-        Optional<Member> member = memberRepository.findByClientId(clientId);
-        if (member.isPresent()) {
+        // 이미 회원인 경우
+        if (memberQueryService.isPresent(clientId)) {
             return AuthResponseDTO.SocialLoginDTO.builder()
                     .message(RE_LOGIN_EXISTING_MEMBER_MESSAGE)
                     .refreshToken(getRefreshToken(loadUserByUsername(clientId)))
                     .build();
         }
+        // 새로 가입한 경우
         return AuthResponseDTO.SocialLoginDTO.builder()
                 .message(TEMPORARY_TOKEN_SUCCESS_MESSAGE)
                 .build();
     }
 
-    @Transactional
     public String getRefreshToken(UserDetails userDetails) {
-        Token token = tokenRepository.findById(userDetails.getUsername()).orElseThrow();
+        Token token = findToken(userDetails.getUsername());
         return token.getRefreshToken();
     }
 
-    @Transactional
     public void deleteRefreshToken(String clientId) {
-        Token token = tokenRepository.findById(clientId).orElseThrow();
-        // todo : 예외처리
+        Token token = findToken(clientId);
         tokenRepository.delete(token);
     }
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String clientId) {
-        Optional<Member> member = memberRepository.findByClientId(clientId);
-        if (member.isPresent()) {
-            return new MemberDetails(member.get());
+        if (memberQueryService.isPresent(clientId)) {
+            return new MemberDetails(memberQueryService.findByClientId(clientId));
         } else {
             return new TemporaryMember(clientId);
         }
+    }
+
+    @Transactional
+    Token findToken(String clientId) {
+        return tokenRepository.findById(clientId).orElseThrow(
+                () -> new GeneralException(ErrorStatus._TOKEN_NOT_FOUND)
+        );
     }
 }
