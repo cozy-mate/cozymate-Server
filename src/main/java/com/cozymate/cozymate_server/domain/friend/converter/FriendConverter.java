2810ed3062782dd1c71b2cbf98ca58d88cd4a2ab
JunhyeokPark(박준혁) <peterp1@inha.edu>
2024-08-06 12:55:29 +0900

    [COZY-146] 친구 목록 조회 API (#24)
    * [COZY-146] feat: 친구 목록 가져오는 API 완성, 잘못된 Swagger 설명 변경
    
    * [COZY-146] feat: 친구 관계 필터링 기능 추가
    
    * [COZY-146] refactor: 표시항목을 Name에서 NickName으로 변경
    
    * [COZY-146] refactor: 피드백 반영(DTO 명, Converter)

--- src/main/java/com/cozymate/cozymate_server/domain/friend/converter/FriendConverter.java
+++ src/main/java/com/cozymate/cozymate_server/domain/friend/converter/FriendConverter.java
@@ -1,7 +1,7 @@
 package com.cozymate.cozymate_server.domain.friend.converter;
 
 import com.cozymate.cozymate_server.domain.friend.Friend;
-import com.cozymate.cozymate_server.domain.friend.dto.FriendRequestDTO;
+import com.cozymate.cozymate_server.domain.friend.dto.FriendResponseDTO.FriendSummaryResponseDTO;
 import com.cozymate.cozymate_server.domain.friend.enums.FriendStatus;
 import com.cozymate.cozymate_server.domain.member.Member;
 
@@ -12,6 +12,13 @@ public class FriendConverter {
             .sender(sender)
             .receiver(receiver)
             .status(FriendStatus.WAITING)
+            .build();
+    }
+
+    public static FriendSummaryResponseDTO toFriendSummaryResponseDTO(Long memberId, String nickname){
+        return FriendSummaryResponseDTO.builder()
+            .memberId(memberId)
+            .nickname(nickname)
             .build();
     }
 
