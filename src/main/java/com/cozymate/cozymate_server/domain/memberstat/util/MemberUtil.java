dcd286573c8c539a74be25352085a9ca0a0c7b46
jpark0506 <peterp1@inha.edu>
2024-08-07 01:03:22 +0900

    [COZY-148] feat: 마법의 숫자 추가

--- src/main/java/com/cozymate/cozymate_server/domain/memberstat/util/MemberUtil.java
+++ src/main/java/com/cozymate/cozymate_server/domain/memberstat/util/MemberUtil.java
@@ -7,46 +7,48 @@ import com.cozymate.cozymate_server.global.utils.TimeUtil;
 public class MemberUtil {
 
     private static final Integer ADDITIONAL_SCORE = 12;
+    private static final Integer NO_SCORE = 0;
     private static final Integer ATTRIBUTE_COUNT = 20;
+    private static final Integer HALF_DIVISION = 2;
+    private static final Integer QUARTER_DIVISION = 4;
+    private static final Integer MULTIPLIER_FOR_PERCENTAGE = 100;
     private static final Integer MAX_SCORE = ADDITIONAL_SCORE * ATTRIBUTE_COUNT;
+
 
     public static MemberStatEqualityResponseDTO toEqualityResponse(MemberStat criteriaMemberStat,
         MemberStat memberStat) {
 
-        int score = 0;
-        score +=
-            criteriaMemberStat.getAcceptance().equals(memberStat.getAcceptance())
-                ? ADDITIONAL_SCORE : 0;
+        int score = NO_SCORE;
+        score += criteriaMemberStat.getAcceptance().equals(memberStat.getAcceptance())
+            ? ADDITIONAL_SCORE : NO_SCORE;
         score += criteriaMemberStat.getAdmissionYear().equals(memberStat.getAdmissionYear())
-            ? ADDITIONAL_SCORE : 0;
-        score += criteriaMemberStat.getMajor().equals(memberStat.getMajor()) ? ADDITIONAL_SCORE : 0;
-        score +=
-            criteriaMemberStat.getSmoking().equals(memberStat.getSmoking()) ? ADDITIONAL_SCORE : 0;
+            ? ADDITIONAL_SCORE : NO_SCORE;
+        score += criteriaMemberStat.getMajor().equals(memberStat.getMajor())
+            ? ADDITIONAL_SCORE : NO_SCORE;
+        score += criteriaMemberStat.getSmoking().equals(memberStat.getSmoking())
+            ? ADDITIONAL_SCORE : NO_SCORE;
         score += criteriaMemberStat.getSleepingHabit().equals(memberStat.getSleepingHabit())
-            ? ADDITIONAL_SCORE : 0;
+            ? ADDITIONAL_SCORE : NO_SCORE;
         score += criteriaMemberStat.getLifePattern().equals(memberStat.getLifePattern())
-            ? ADDITIONAL_SCORE : 0;
-        score +=
-            criteriaMemberStat.getIntimacy().equals(memberStat.getIntimacy()) ? ADDITIONAL_SCORE
-                : 0;
-        score +=
-            criteriaMemberStat.getCanShare().equals(memberStat.getCanShare()) ? ADDITIONAL_SCORE
-                : 0;
-        score +=
-            criteriaMemberStat.getIsPlayGame().equals(memberStat.getIsPlayGame()) ? ADDITIONAL_SCORE
-                : 0;
+            ? ADDITIONAL_SCORE : NO_SCORE;
+        score += criteriaMemberStat.getIntimacy().equals(memberStat.getIntimacy())
+            ? ADDITIONAL_SCORE : NO_SCORE;
+        score += criteriaMemberStat.getCanShare().equals(memberStat.getCanShare())
+            ? ADDITIONAL_SCORE : NO_SCORE;
+        score += criteriaMemberStat.getIsPlayGame().equals(memberStat.getIsPlayGame())
+            ? ADDITIONAL_SCORE : NO_SCORE;
         score += criteriaMemberStat.getIsPhoneCall().equals(memberStat.getIsPhoneCall())
-            ? ADDITIONAL_SCORE : 0;
-        score +=
-            criteriaMemberStat.getStudying().equals(memberStat.getStudying()) ? ADDITIONAL_SCORE
-                : 0;
-        score +=
-            criteriaMemberStat.getIntake().equals(memberStat.getIntake()) ? ADDITIONAL_SCORE : 0;
+            ? ADDITIONAL_SCORE : NO_SCORE;
+        score += criteriaMemberStat.getStudying().equals(memberStat.getStudying())
+            ? ADDITIONAL_SCORE : NO_SCORE;
+        score += criteriaMemberStat.getIntake().equals(memberStat.getIntake())
+            ? ADDITIONAL_SCORE : NO_SCORE;
         score += criteriaMemberStat.getCleaningFrequency().equals(memberStat.getCleaningFrequency())
-            ? ADDITIONAL_SCORE : 0;
+            ? ADDITIONAL_SCORE : NO_SCORE;
         score += criteriaMemberStat.getPersonality().equals(memberStat.getPersonality())
-            ? ADDITIONAL_SCORE : 0;
-        score += criteriaMemberStat.getMbti().equals(memberStat.getMbti()) ? ADDITIONAL_SCORE : 0;
+            ? ADDITIONAL_SCORE : NO_SCORE;
+        score += criteriaMemberStat.getMbti().equals(memberStat.getMbti())
+            ? ADDITIONAL_SCORE : NO_SCORE;
 
         score += calculateTimeScore(criteriaMemberStat.getWakeUpTime(), memberStat.getWakeUpTime());
         score += calculateTimeScore(criteriaMemberStat.getSleepingTime(),
@@ -59,7 +61,7 @@ public class MemberUtil {
         score += calculateSensitivityScore(criteriaMemberStat.getNoiseSensitivity(),
             memberStat.getNoiseSensitivity());
 
-        double percent = (double) score / MAX_SCORE * 100;
+        double percent = (double) score / MAX_SCORE * MULTIPLIER_FOR_PERCENTAGE;
 
         return MemberStatEqualityResponseDTO.builder()
             .memberId(memberStat.getMember().getId())
@@ -76,9 +78,9 @@ public class MemberUtil {
         int timeDifference = Math.abs(time1 - time2);
         return switch (timeDifference) {
             case 0 -> ADDITIONAL_SCORE;
-            case 1 -> ADDITIONAL_SCORE / 2;
-            case 2 -> ADDITIONAL_SCORE / 4;
-            default -> 0;
+            case 1 -> ADDITIONAL_SCORE / HALF_DIVISION;
+            case 2 -> ADDITIONAL_SCORE / QUARTER_DIVISION;
+            default -> NO_SCORE;
         };
     }
 
@@ -87,8 +89,8 @@ public class MemberUtil {
         int sensitivityDifference = Math.abs(sensitivity1 - sensitivity2);
         return switch (sensitivityDifference) {
             case 0 -> ADDITIONAL_SCORE;
-            case 1 -> ADDITIONAL_SCORE / 2;
-            default -> 0;
+            case 1 -> ADDITIONAL_SCORE / HALF_DIVISION;
+            default -> NO_SCORE;
         };
     }
 
