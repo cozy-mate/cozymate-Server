1d8cbe542c3b7ff0120d57f73fa31fac60397d16
subin <118423039+suuu0719@users.noreply.github.com>
2024-08-08 19:21:59 +0900

    [COZY-80] 방 삭제 API  (#23)
    * [COZY-80] feature: 방 삭제 기능
    
    * [COZY-80] fix: 방 삭제 기능 수정
    
    * [COZY-80] fix: 방 삭제 기능 수정
    
    * [COZY-79] fix: ErrorStatus 수정
    
    * [COZY-80] fix: 삭제하려는 사용자 검증과정 추가
    
    * [COZY-80] 충돌 해결
    
    * [COZY-80] fix: 피드백 반영
    
    * [COZY-80] fix: 피드백 반영
    
    * [COZY-80] 충돌 수정
    
    * [COZY-80] fix: 피드백 반영
    
    * [COZY-80] fix: 주석 제거

--- src/main/java/com/cozymate/cozymate_server/domain/post/Post.java
+++ src/main/java/com/cozymate/cozymate_server/domain/post/Post.java
@@ -35,4 +35,5 @@ public class Post extends BaseTimeEntity {
     private String title;
 
     private String content;
+
 }
--- src/main/java/com/cozymate/cozymate_server/domain/post/PostRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/post/PostRepository.java
@@ -1,7 +1,10 @@
 package com.cozymate.cozymate_server.domain.post;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByFeedId(Long feedId);
+    void deleteByFeedId(Long feedId);
 
 }
