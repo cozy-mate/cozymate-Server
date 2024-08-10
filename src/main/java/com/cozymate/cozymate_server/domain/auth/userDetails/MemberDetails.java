f27233e3e28004d083859a08aaf7c1c876e7a05e
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-08-10 16:33:28 +0900

    [COZY-163] refactor : record 방식 사용 안함

--- src/main/java/com/cozymate/cozymate_server/domain/auth/userDetails/MemberDetails.java
+++ src/main/java/com/cozymate/cozymate_server/domain/auth/userDetails/MemberDetails.java
@@ -3,15 +3,16 @@ package com.cozymate.cozymate_server.domain.auth.userDetails;
 import com.cozymate.cozymate_server.domain.member.Member;
 
 import java.util.Collection;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@Getter
 @RequiredArgsConstructor
-public record MemberDetails(Member member) implements UserDetails {
+@Getter
+public class MemberDetails implements UserDetails {
+    private final Member member;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return member.getRole().getAuthorities();
