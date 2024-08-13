41b0e9d283d7ccbac647f1f20a8638da2db02b62
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-08-14 02:33:19 +0900

    [COZY-173] refactor : 토큰만 주는 DTO 생성

--- src/main/java/com/cozymate/cozymate_server/domain/auth/dto/AuthResponseDTO.java
+++ src/main/java/com/cozymate/cozymate_server/domain/auth/dto/AuthResponseDTO.java
@@ -1,7 +1,5 @@
 package com.cozymate.cozymate_server.domain.auth.dto;
 
-import com.cozymate.cozymate_server.domain.member.dto.MemberResponseDTO;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,27 +7,11 @@ import lombok.NoArgsConstructor;
 
 public class AuthResponseDTO {
 
-    @Builder
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class UrlDTO {
-        String redirectUrl;
-    }
-
-
     /**
      * {
      *   "message": "임시토큰 발급완료" or "기존회원 로그인",
      *   "accessToken": "accesstokenheader.accesstokenpayload.accesstokensignature",
      *   "refreshToken": null or "refreshtokenheader.refreshtokenpayload.refreshtokensignatre",
-     *   "memberInfoDTO": {
-     *     "name": "김수환",
-     *     "nickname": "말즈",
-     *     "gender": "MALE",
-     *     "birthday": "2000-01-20",
-     *     "persona": 1
-     *   }
      * }
      */
     @Builder
@@ -40,7 +22,6 @@ public class AuthResponseDTO {
         String message;
         String accessToken;
         String refreshToken;
-        MemberResponseDTO.MemberInfoDTO memberInfoDTO;
     }
 
 }
