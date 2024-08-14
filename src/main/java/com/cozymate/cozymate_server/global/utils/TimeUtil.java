ccfe317ac42500aa7c1e0f07940a3347ac1c7f1b
JunhyeokPark(박준혁) <peterp1@inha.edu>
2024-08-14 23:42:32 +0900

    [COZY-178] feat: MemberStat ResponseType 번경 (#55)

--- src/main/java/com/cozymate/cozymate_server/global/utils/TimeUtil.java
+++ src/main/java/com/cozymate/cozymate_server/global/utils/TimeUtil.java
@@ -3,12 +3,14 @@ package com.cozymate.cozymate_server.global.utils;
 import com.cozymate.cozymate_server.global.response.code.status.ErrorStatus;
 import com.cozymate.cozymate_server.global.response.exception.GeneralException;
 import java.time.LocalDate;
+import java.util.HashMap;
 
 public class TimeUtil {
 
     private static final String MORNING = "오전";
     private static final String AFTERNOON = "오후";
     private static final Integer HALFDAY = 12;
+    private static final Integer STARTDAY = 0;
 
     public static Integer convertTime(String meridian, Integer time){
         switch (meridian){
@@ -27,6 +29,25 @@ public class TimeUtil {
             default:
                 throw new GeneralException(ErrorStatus._MEMBERSTAT_MERIDIAN_NOT_VALID);
         }
+    }
+
+    public static String convertToMeridian(Integer time){
+        if(time < HALFDAY){
+            return MORNING;
+        } else if(time.equals(HALFDAY)){
+            return AFTERNOON;
+        }
+        return AFTERNOON;
+    }
+
+    public static Integer convertToTime(Integer time){
+        if(time.equals(STARTDAY)){
+            return HALFDAY;
+        }
+        else if(time <= HALFDAY){
+            return time;
+        }
+        return time - HALFDAY;
     }
 
     public static Integer calculateAge(LocalDate birthDate) {
