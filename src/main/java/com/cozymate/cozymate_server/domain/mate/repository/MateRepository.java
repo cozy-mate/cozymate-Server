6213f876f04e08ab914c2ff8910827284ef1fcc0
Ga Dong Sik <62535229+eple0329@users.noreply.github.com>
2024-08-21 13:06:32 +0900

    [COZY-206] feat: 이벤트로 특정 시간에 완료되지 않은 Role RoomLog에 추가, 생일, BestCozymate 기능 Roomlog 추가
    * [COZY-206] feat: 이벤트로 특정 시간에 완료되지 않은 Role RoomLog에 추가, 생일, BestCozymate 기능 Roomlog 추가
    
    * [COZY-206] fix: 에러 해결 및 연결 완료

--- src/main/java/com/cozymate/cozymate_server/domain/mate/repository/MateRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/mate/repository/MateRepository.java
@@ -3,6 +3,7 @@ package com.cozymate.cozymate_server.domain.mate.repository;
 import com.cozymate.cozymate_server.domain.mate.Mate;
 import com.cozymate.cozymate_server.domain.mate.enums.EntryStatus;
 import com.cozymate.cozymate_server.domain.room.enums.RoomStatus;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,25 +11,38 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MateRepository extends JpaRepository<Mate, Long> {
+
     List<Mate> findByRoomId(Long roomId);
+
     void deleteByRoomId(Long roomId);
+
     Optional<Mate> findByRoomIdAndIsRoomManager(Long roomId, boolean isRoomManager);
+
     Optional<Mate> findByRoomIdAndMemberId(Long roomId, Long memberId);
 
     Long countByRoomId(Long roomId);
 
     @Query("SELECT COUNT(m) > 0 FROM Mate m WHERE m.member.id = :memberId AND (m.room.status = :status1 OR m.room.status = :status2)")
-    boolean existsByMemberIdAndRoomStatuses(@Param("memberId") Long memberId, @Param("status1") RoomStatus status1, @Param("status2") RoomStatus status2);
+    boolean existsByMemberIdAndRoomStatuses(@Param("memberId") Long memberId,
+        @Param("status1") RoomStatus status1, @Param("status2") RoomStatus status2);
 
     Optional<Mate> findByMemberIdAndRoomId(Long MemberId, Long RoomId);
 
     Optional<Mate> findByMemberIdAndEntryStatus(Long memberId, EntryStatus entryStatus);
 
-    boolean existsByMemberIdAndEntryStatusAndRoomStatusIn(Long memberId, EntryStatus entryStatus, List<RoomStatus> roomStatuses);
+    boolean existsByMemberIdAndEntryStatusAndRoomStatusIn(Long memberId, EntryStatus entryStatus,
+        List<RoomStatus> roomStatuses);
 
     boolean existsByMemberIdAndRoomId(Long MemberId, Long RoomId);
 
-    Optional<Mate> findByMemberIdAndEntryStatusAndRoomStatusIn(Long memberId, EntryStatus entryStatus, List<RoomStatus> roomStatuses);
+    Optional<Mate> findByMemberIdAndEntryStatusAndRoomStatusIn(Long memberId,
+        EntryStatus entryStatus, List<RoomStatus> roomStatuses);
 
     List<Mate> findAllByRoomIdAndEntryStatus(Long roomId, EntryStatus entryStatus);
+
+    List<Mate> findAllByMemberBirthDayAndEntryStatus(LocalDate birthday, EntryStatus entryStatus);
+
+    // MemberBirthDay의 Localdate 값에서 Month와 Day가 같은 Member들을 찾는다.
+    @Query("SELECT m FROM Mate m WHERE MONTH(m.member.birthDay) = :month AND DAY(m.member.birthDay) = :day AND m.entryStatus = :entryStatus")
+    List<Mate> findAllByMemberBirthDayMonthAndDayAndEntryStatus(@Param("month") int month, @Param("day") int day, @Param("entryStatus") EntryStatus entryStatus);
 }
--- src/main/java/com/cozymate/cozymate_server/domain/roomlog/service/RoomLogCommandService.java
+++ src/main/java/com/cozymate/cozymate_server/domain/roomlog/service/RoomLogCommandService.java
@@ -1,10 +1,12 @@
 package com.cozymate.cozymate_server.domain.roomlog.service;
 
+import com.cozymate.cozymate_server.domain.mate.Mate;
 import com.cozymate.cozymate_server.domain.room.Room;
 import com.cozymate.cozymate_server.domain.roomlog.RoomLog;
 import com.cozymate.cozymate_server.domain.roomlog.converter.RoomLogConverter;
 import com.cozymate.cozymate_server.domain.roomlog.repository.RoomLogRepository;
 import com.cozymate.cozymate_server.domain.todo.Todo;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -21,13 +23,20 @@ public class RoomLogCommandService {
 
     private final RoomLogRepository roomLogRepository;
 
+    // 투두 완료했을 때 메시지
     private static final String DEFAULT_FINAL_MESSAGE = "완료했어요!";
     private static final List<String> FINISH_MESSAGE_LIST = Arrays.asList(
         "완료하여, cozy room을 만드는데 기여했어요!",
         "완료했어요! 얼른 칭찬해주세요!",
         "완료하여, 최고의 cozy mate가 되었어요!"
     );
+
+    // 방이 생성되었을 때 메시지
     public static final String DEFAULT_CREATION_MESSAGE = "의 역사적인 하루가 시작됐어요!";
+
+    public static final String DEFAULT_REMINDING_ROLE_MESSAGE = "까먹은 거 같아요 ㅠㅠ";
+
+    public static final String DEFAULT_CHOICE_COZY_MATE_MESSAGE = "의 Best, Worst 코지메이트를 선정해주세요!";
 
     // 투두 추가되었을 때
     public void addRoomLogFromTodo(Todo todo) {
@@ -57,6 +66,28 @@ public class RoomLogCommandService {
     public void addRoomLogCreationRoom(Room room) {
         String content = "{" + room.getName() + "}" + DEFAULT_CREATION_MESSAGE;
         roomLogRepository.save(RoomLogConverter.toEntity(content, room, null));
+    }
+
+    // 역할을 특정 시간에 수행하지 않았을 때 룸 로그 추가
+    public void addRoomLogRemindingRole(Todo todo) {
+        String who = "{" + todo.getMate().getMember().getNickname() + "}님이 ";
+        String what = "[" + todo.getContent() + "]을/를 " + DEFAULT_REMINDING_ROLE_MESSAGE;
+        String content = who + what;
+        roomLogRepository.save(RoomLogConverter.toEntity(content, todo.getRoom(), null));
+    }
+
+    // 이달의 베스트 코지메이트, 워스크 코지메이트 선정 알림 로그 추가
+    public void addRoomLogChoiceCozyMate(Room room, String month) {
+        // [해당 월]의 Best, Worst 코지메이트를 선정해주세요!
+        String content = month + DEFAULT_CHOICE_COZY_MATE_MESSAGE;
+        roomLogRepository.save(RoomLogConverter.toEntity(content, room, null));
+    }
+
+    public void addRoomLogBirthday(Mate mate, LocalDate today) {
+        // ”0월 0일은 [닉네임]님의 생일이에요! 모두 축하해주세요!”
+        String content = today.getMonthValue() + "월 " + today.getDayOfMonth() + "일은 "
+            + mate.getMember().getNickname() + "님의 생일이에요! 모두 축하해주세요!";
+        roomLogRepository.save(RoomLogConverter.toEntity(content, mate.getRoom(), null));
     }
 
 }
