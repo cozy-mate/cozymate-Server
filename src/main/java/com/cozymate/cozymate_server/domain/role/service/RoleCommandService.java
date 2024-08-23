9b26d961bef6df98e0246cb70b0bd454534bc272
Ga Dong Sik <62535229+eple0329@users.noreply.github.com>
2024-08-23 12:28:08 +0900

    [COZY-209] feat: 오늘 Role을 생성했다면 Todo도 추가

--- src/main/java/com/cozymate/cozymate_server/domain/role/service/RoleCommandService.java
+++ src/main/java/com/cozymate/cozymate_server/domain/role/service/RoleCommandService.java
@@ -8,8 +8,12 @@ import com.cozymate.cozymate_server.domain.role.converter.RoleConverter;
 import com.cozymate.cozymate_server.domain.role.dto.RoleRequestDto.CreateRoleRequestDto;
 import com.cozymate.cozymate_server.domain.role.enums.DayListBitmask;
 import com.cozymate.cozymate_server.domain.role.repository.RoleRepository;
+import com.cozymate.cozymate_server.domain.todo.converter.TodoConverter;
+import com.cozymate.cozymate_server.domain.todo.repository.TodoRepository;
 import com.cozymate.cozymate_server.global.response.code.status.ErrorStatus;
 import com.cozymate.cozymate_server.global.response.exception.GeneralException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,45 +23,63 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RoleCommandService {
 
-    private final RoleRepository roleRepository;
-    private final MateRepository mateRepository;
+        private final RoleRepository roleRepository;
+        private final MateRepository mateRepository;
+        private final TodoRepository todoRepository;
 
 
-    @Transactional
-    public void createRole(
-        Member member,
-        Long roomId,
-        CreateRoleRequestDto requestDto
-    ) {
-        // 해당 API를 호출한 사람
-        Mate mate = mateRepository.findByMemberIdAndRoomId(member.getId(), roomId)
-            .orElseThrow(() -> new GeneralException(ErrorStatus._MATE_OR_ROOM_NOT_FOUND));
+        @Transactional
+        public void createRole(
+                Member member,
+                Long roomId,
+                CreateRoleRequestDto requestDto
+        ) {
+                // 해당 API를 호출한 사람
+                Mate mate = mateRepository.findByMemberIdAndRoomId(member.getId(), roomId)
+                        .orElseThrow(
+                                () -> new GeneralException(ErrorStatus._MATE_OR_ROOM_NOT_FOUND));
 
-        List<DayListBitmask> repeatDayList = requestDto.getRepeatDayList().stream()
-            .map(DayListBitmask::valueOf).toList();
-        int repeatDayBitmast = RoleConverter.convertDayListToBitmask(repeatDayList);
+                List<DayListBitmask> repeatDayList = requestDto.getRepeatDayList().stream()
+                        .map(DayListBitmask::valueOf).toList();
+                int repeatDayBitmast = RoleConverter.convertDayListToBitmask(repeatDayList);
 
-        // Role의 대상이 되는 사람
-        requestDto.getMateIdList().forEach(mateId -> {
-            Mate targerMate = mateRepository.findById(mateId) // TODO: mate를 한번에 가져오는 방식으로 변경해야함
-                .orElseThrow(() -> new GeneralException(ErrorStatus._MATE_NOT_FOUND));
-            Role role = RoleConverter.toEntity(targerMate, requestDto.getTitle(), repeatDayBitmast);
-            roleRepository.save(role);
-        });
-    }
+                // 오늘 요일 가져오기
+                DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+                int dayBitmask = DayListBitmask.getBitmaskByDayOfWeek(dayOfWeek);
 
-    public void deleteRole(Long roomId, Long roleId, Member member) {
-        Mate mate = mateRepository.findByMemberIdAndRoomId(member.getId(), roomId)
-            .orElseThrow(() -> new GeneralException(ErrorStatus._MATE_OR_ROOM_NOT_FOUND));
-
-        Role roleToDelete = roleRepository.findById(roleId)
-            .orElseThrow(() -> new GeneralException(ErrorStatus._ROLE_NOT_FOUND));
-
-        if (Boolean.FALSE.equals(
-            member.getId().equals(roleToDelete.getMate().getMember().getId())
-        )) {
-            throw new GeneralException(ErrorStatus._ROLE_MATE_MISMATCH);
+                // Role의 대상이 되는 사람
+                requestDto.getMateIdList().forEach(mateId -> {
+                        Mate targerMate = mateRepository.findById(
+                                        mateId) // TODO: mate를 한번에 가져오는 방식으로 변경해야함
+                                .orElseThrow(
+                                        () -> new GeneralException(ErrorStatus._MATE_NOT_FOUND));
+                        Role role = RoleConverter.toEntity(targerMate, requestDto.getTitle(),
+                                repeatDayBitmast);
+                        if ((role.getRepeatDays() & dayBitmask) != 0) {
+                                todoRepository.save(
+                                        TodoConverter.toEntity(role.getMate().getRoom(),
+                                                role.getMate(),
+                                                role.getContent(),
+                                                LocalDate.now(), role)
+                                );
+                        }
+                        roleRepository.save(role);
+                });
         }
-        roleRepository.delete(roleToDelete);
-    }
+
+        public void deleteRole(Long roomId, Long roleId, Member member) {
+                Mate mate = mateRepository.findByMemberIdAndRoomId(member.getId(), roomId)
+                        .orElseThrow(
+                                () -> new GeneralException(ErrorStatus._MATE_OR_ROOM_NOT_FOUND));
+
+                Role roleToDelete = roleRepository.findById(roleId)
+                        .orElseThrow(() -> new GeneralException(ErrorStatus._ROLE_NOT_FOUND));
+
+                if (Boolean.FALSE.equals(
+                        member.getId().equals(roleToDelete.getMate().getMember().getId())
+                )) {
+                        throw new GeneralException(ErrorStatus._ROLE_MATE_MISMATCH);
+                }
+                roleRepository.delete(roleToDelete);
+        }
 }
