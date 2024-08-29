7c424c1a242d174cd95d3384f6cef9be51a07ef8
veronees <jsg6854@naver.com>
2024-08-30 03:53:58 +0900

    [COZY-229] fix : 오류 수정

--- src/main/java/com/cozymate/cozymate_server/domain/chat/dto/ChatRequestDto.java
+++ src/main/java/com/cozymate/cozymate_server/domain/chat/dto/ChatRequestDto.java
@@ -3,9 +3,11 @@ package com.cozymate.cozymate_server.domain.chat.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChatRequestDto {
     @NotBlank
     private String content;
