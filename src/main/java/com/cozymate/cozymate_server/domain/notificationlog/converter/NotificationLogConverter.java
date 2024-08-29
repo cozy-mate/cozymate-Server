34b2fd0b66a688807d128e355c8ff373a92a126a
Ga Dong Sik <62535229+eple0329@users.noreply.github.com>
2024-08-30 03:02:34 +0900

    [COZY-230] feat: 카테고리 반환 추가

--- src/main/java/com/cozymate/cozymate_server/domain/notificationlog/converter/NotificationLogConverter.java
+++ src/main/java/com/cozymate/cozymate_server/domain/notificationlog/converter/NotificationLogConverter.java
@@ -7,7 +7,8 @@ import java.time.format.DateTimeFormatter;
 
 public class NotificationLogConverter {
 
-    public static NotificationLogResponseDto toResponseDto(String content, LocalDateTime createdAt) {
+    public static NotificationLogResponseDto toResponseDto(String content, LocalDateTime createdAt,
+        String category) {
         Duration duration = Duration.between(createdAt, LocalDateTime.now());
         long minutes = duration.toMinutes();
         long hours = duration.toHours();
@@ -15,7 +16,7 @@ public class NotificationLogConverter {
         String formattedCreatedAt;
 
         if (minutes < 60) { // 1분 ~ 59분 전
-            formattedCreatedAt =  minutes + "분 전";
+            formattedCreatedAt = minutes + "분 전";
         } else if (hours < 24) { // 1시간 ~ 23시간 전
             formattedCreatedAt = hours + "시간 전";
         } else if (days <= 3) { // 1일 ~ 3일 전
@@ -27,6 +28,7 @@ public class NotificationLogConverter {
         return NotificationLogResponseDto.builder()
             .content(content)
             .createdAt(formattedCreatedAt)
+            .category(category)
             .build();
     }
 }
