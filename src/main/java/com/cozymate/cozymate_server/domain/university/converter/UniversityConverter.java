41b7922358c4c76ee6ee01c9e57ad0621315d153
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-10-30 16:13:05 +0900

    [COZY-284] feat : 대학교 목록 조회시 기본 정보 목록 dto convert,

--- src/main/java/com/cozymate/cozymate_server/domain/university/converter/UniversityConverter.java
+++ src/main/java/com/cozymate/cozymate_server/domain/university/converter/UniversityConverter.java
@@ -3,6 +3,8 @@ package com.cozymate.cozymate_server.domain.university.converter;
 import com.cozymate.cozymate_server.domain.university.University;
 import com.cozymate.cozymate_server.domain.university.dto.UniversityRequest;
 import com.cozymate.cozymate_server.domain.university.dto.UniversityResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UniversityConverter {
     public static University toUniversity(UniversityRequest.UniversityDTO universityDTO){
@@ -15,6 +17,7 @@ public class UniversityConverter {
 
     public static UniversityResponse.UniversityDTO toUniversityDTO(University university){
         return UniversityResponse.UniversityDTO.builder()
+                .id(university.getId())
                 .name(university.getName())
                 .mailPattern(university.getMailPattern())
                 .dormitoryNames(university.getDormitoryNames())
@@ -22,6 +25,22 @@ public class UniversityConverter {
                 .build();
     }
 
+    public static UniversityResponse.UniversityBasicInfoDTO toUniversityBasicInfoDTO(University university){
+        return UniversityResponse.UniversityBasicInfoDTO.builder()
+                .id(university.getId())
+                .name(university.getName())
+                .build();
+    }
+    public static UniversityResponse.UniversityListDTO toUniversityListDTO(List<University> universityList){
+        List<UniversityResponse.UniversityBasicInfoDTO> universityDTOList = universityList.stream()
+                .map(UniversityConverter::toUniversityBasicInfoDTO)
+                .collect(Collectors.toList());
+
+        return UniversityResponse.UniversityListDTO.builder()
+                .universityList(universityDTOList)
+                .build();
+
+    }
 
 
 }
