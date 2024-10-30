3fa57f57528ff8b807674160386f02be78051c6b
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-10-30 16:12:18 +0900

    [COZY-284] feat : 대학교 목록 조회 api 새성

--- src/main/java/com/cozymate/cozymate_server/domain/university/dto/UniversityResponse.java
+++ src/main/java/com/cozymate/cozymate_server/domain/university/dto/UniversityResponse.java
@@ -1,6 +1,5 @@
 package com.cozymate.cozymate_server.domain.university.dto;
 
-import com.cozymate.cozymate_server.domain.university.dto.UniversityRequest.UniversityDTO;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,9 +12,26 @@ public class UniversityResponse {
     @AllArgsConstructor
     @Builder
     public static class UniversityDTO{
+        private Long id;
         private String name;
         private String mailPattern;
         private List<String> dormitoryNames;
         private List<String> departments;
+    }
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class UniversityBasicInfoDTO{
+        private String name;
+        private Long id;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class UniversityListDTO{
+        private List<UniversityBasicInfoDTO> universityList;
     }
 }
