011131d3aa2a51904be9507b53f174f2263f504e
jpark0506 <peterp1@inha.edu>
2024-10-31 00:30:40 +0900

    [COZY-311] feat: 주석 추가 완료

--- src/main/java/com/cozymate/cozymate_server/domain/memberstat/dto/MemberStatDifferenceResponseDTO.java
+++ src/main/java/com/cozymate/cozymate_server/domain/memberstat/dto/MemberStatDifferenceResponseDTO.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 @Builder
 public class MemberStatDifferenceResponseDTO {
 
+    // BLUE : 모두 일치하는 일치율 항목
+    // RED : 모두 다른 일치율 항목
+    // WHITE : 모두 같지도, 모두 다르지도 않는 일치율 항목
     private List<String> blue;
     private List<String> red;
     private List<String> white;
