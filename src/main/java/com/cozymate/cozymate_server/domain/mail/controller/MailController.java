9969f1cbcfd6632ae1e08c46a462fa90ee023ceb
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-10-30 16:15:33 +0900

    [COZY-284] feat : 메일 인증 api description 추가

--- src/main/java/com/cozymate/cozymate_server/domain/mail/controller/MailController.java
+++ src/main/java/com/cozymate/cozymate_server/domain/mail/controller/MailController.java
@@ -3,14 +3,15 @@ package com.cozymate.cozymate_server.domain.mail.controller;
 
 import com.cozymate.cozymate_server.domain.auth.dto.AuthResponseDTO;
 import com.cozymate.cozymate_server.domain.auth.userDetails.MemberDetails;
-import com.cozymate.cozymate_server.domain.mail.dto.MailResponse;
+import com.cozymate.cozymate_server.domain.mail.dto.MailRequest;
 import com.cozymate.cozymate_server.domain.mail.service.MailService;
 import com.cozymate.cozymate_server.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,22 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class MailController {
     private final MailService mailService;
 
-    @PostMapping("/{mail-address}")
-    public ResponseEntity sendUniversityAuthenticationCode(
+    @PostMapping("/")
+    @Operation(summary = "[말즈] 메일 보내기 API",
+            description = "request body :  <br>"
+                    + "mailAddress : 12345@inha.edu <br>"
+                    + "universityId : 1 <br>")
+    public ResponseEntity<Void> sendUniversityAuthenticationCode(
             @AuthenticationPrincipal MemberDetails memberDetails,
-            @PathVariable("mail-address") String mailAddress
+            @RequestBody MailRequest.SendDTO sendDTO
     ) {
-        mailService.sendUniversityAuthenticationCode(memberDetails, mailAddress);
+        mailService.sendUniversityAuthenticationCode(memberDetails, sendDTO);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/verify/{code}")
-    public ResponseEntity<ApiResponse<AuthResponseDTO.TokenResponseDTO>> verifyAuthenticationCode(
+    @PostMapping("/verify/")
+    @Operation(summary = "[말즈] 메일 인증 API",
+            description = "request body :  <br>"
+                    + " code : a1B2c3 <br>"
+                    + " universityId : 1 <br>"
+                    + "response :"
+                    + " 토큰")
+    public ResponseEntity<ApiResponse<AuthResponseDTO.TokenResponseDTO>> verifyMemberUniversity(
             @AuthenticationPrincipal MemberDetails memberDetails,
-            @PathVariable("code") String code
+            @RequestBody MailRequest.VerifyDTO verifyDTO
 
-    ) {
-        return ResponseEntity.ok(ApiResponse.onSuccess(mailService.verifyAuthenticationCode(memberDetails, code)));
+            ) {
+        return ResponseEntity.ok(ApiResponse.onSuccess(mailService.verifyMemberUniversity(memberDetails,verifyDTO)));
     }
 
 }
