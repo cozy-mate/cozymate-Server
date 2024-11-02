afad050d9c542ab1f785d32dba7941000d33937a
veronees <jsg6854@naver.com>
2024-11-03 04:42:03 +0900

    [COZY-294] fix : 테스트용 컨트롤러 삭제

--- src/main/java/com/cozymate/cozymate_server/domain/notificationlog/controller/NotificationLogController.java
+++ src/main/java/com/cozymate/cozymate_server/domain/notificationlog/controller/NotificationLogController.java
@@ -1,8 +1,6 @@
 package com.cozymate.cozymate_server.domain.notificationlog.controller;
 
 import com.cozymate.cozymate_server.domain.auth.userDetails.MemberDetails;
-import com.cozymate.cozymate_server.domain.fcm.Fcm;
-import com.cozymate.cozymate_server.domain.fcm.repository.FcmRepository;
 import com.cozymate.cozymate_server.domain.notificationlog.dto.NotificationLogResponseDto;
 import com.cozymate.cozymate_server.domain.notificationlog.service.NotificationLogCommandService;
 import com.cozymate.cozymate_server.domain.notificationlog.service.NotificationLogQueryService;
@@ -14,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationLogController {
 
     private final NotificationLogQueryService notificationLogQueryService;
-    private final NotificationLogCommandService notificationLogCommandService;
 
     @GetMapping
     @Operation(summary = "[베로] 알림 내역 조회", description = "")
@@ -33,18 +29,5 @@ public class NotificationLogController {
         @AuthenticationPrincipal MemberDetails memberDetails) {
         return ResponseEntity.ok(ApiResponse.onSuccess(
             notificationLogQueryService.getNotificationLogList(memberDetails.getMember())));
-    }
-
-    @Deprecated
-    @PostMapping
-    @Operation(summary = "[베로] 공지 사항 테스트", description = "")
-    public ResponseEntity<ApiResponse<String>> sendNotice() {
-
-        String content = "이게 머여.. 어억? 오옹? 치킨..? 이거 cozymate 출시 이벤트 아니에유?";
-        Long noticeId = 1L;
-
-        notificationLogCommandService.saveNoticeNotificationLog(content, noticeId);
-
-        return ResponseEntity.ok(ApiResponse.onSuccess("공지 완료"));
     }
 }
