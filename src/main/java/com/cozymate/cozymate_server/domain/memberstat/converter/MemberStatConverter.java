3bc895bd03031d00748a9c04d5a2daa4be8774fd
jpark0506 <peterp1@inha.edu>
2024-11-03 22:21:09 +0900

    [COZY-309] fix: merge conflict 제거

--- src/main/java/com/cozymate/cozymate_server/domain/memberstat/converter/MemberStatConverter.java
+++ src/main/java/com/cozymate/cozymate_server/domain/memberstat/converter/MemberStatConverter.java
@@ -212,7 +212,6 @@ public class MemberStatConverter {
                         break;
                 }
             }
-        }
 
         return MemberStatDifferenceResponseDTO.builder()
             .blue(blue)
