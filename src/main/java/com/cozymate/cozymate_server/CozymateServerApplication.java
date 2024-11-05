212fe159b14fd6e0a7af50115e6cb862b9bc19e1
Ga Dong Sik <ret0422@gmail.com>
2024-11-05 12:26:45 +0900

    [COZY-301] AOP Function Call Log 추가

--- src/main/java/com/cozymate/cozymate_server/CozymateServerApplication.java
+++ src/main/java/com/cozymate/cozymate_server/CozymateServerApplication.java
@@ -2,12 +2,14 @@ package com.cozymate.cozymate_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class CozymateServerApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/cozymate/cozymate_server/global/logging/AopLogger.java
@@ -0,0 +1,32 @@
+package com.cozymate.cozymate_server.global.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class AopLogger {
+
+    @Pointcut("execution(* com.cozymate.cozymate_server.domain..*.*(..)) && !within(com.cozymate.cozymate_server.domain.auth..*)")
+    public void classAdvice() {
+    }
+
+    @Before("classAdvice()")
+    public void beforeClassCall(JoinPoint joinPoint) {
+        if(MDC.get("startTime") != null){
+            long startTime = Long.parseLong(MDC.get("startTime"));
+            long endTime = System.currentTimeMillis();
+            String functionName = joinPoint.getSignature().getName();
+            log.info("REQUIESID: {}, FUNC: {}, TIME: {}ms",
+                MDC.get("requestId"), functionName, endTime - startTime
+            );
+        }
+
+    }
+}
