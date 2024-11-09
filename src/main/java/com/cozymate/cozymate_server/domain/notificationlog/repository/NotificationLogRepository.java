9f26cb4ea6679efbde59cc687528f80920a58086
veronees <jsg6854@naver.com>
2024-11-09 17:37:21 +0900

    [COZY-342] fix: NotificationLog 도메인 dto 클래스 -> record 변경

--- src/main/java/com/cozymate/cozymate_server/domain/notificationlog/repository/NotificationLogRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/notificationlog/repository/NotificationLogRepository.java
@@ -2,13 +2,10 @@ package com.cozymate.cozymate_server.domain.notificationlog.repository;
 
 import com.cozymate.cozymate_server.domain.member.Member;
 import com.cozymate.cozymate_server.domain.notificationlog.NotificationLog;
-import com.cozymate.cozymate_server.domain.notificationlog.enums.NotificationType.NotificationCategory;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationLogRepository extends JpaRepository<NotificationLog, Long> {
-
-    List<NotificationLog> findByMemberAndCategoryOrderByIdDesc(Member member, NotificationCategory notificationCategory);
 
     List<NotificationLog> findByMemberOrderByIdDesc(Member member);
 }
