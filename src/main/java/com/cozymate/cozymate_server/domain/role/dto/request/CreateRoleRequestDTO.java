367945fae81c3ae907cb8a1953102715325d797a
Ga Dong Sik <ret0422@gmail.com>
2024-11-09 16:18:01 +0900

    [COZY-324] refac: Todo, Role Rule 엔티티 Varchar 사이즈 수정

--- src/main/java/com/cozymate/cozymate_server/domain/role/dto/request/CreateRoleRequestDTO.java
+++ src/main/java/com/cozymate/cozymate_server/domain/role/dto/request/CreateRoleRequestDTO.java
@@ -10,9 +10,9 @@ public record CreateRoleRequestDTO(
     @NotEmpty(message = "mateIdList는 비어있을 수 없습니다.")
     List<Long> mateIdList,
 
-    @NotEmpty(message = "title은 비어있을 수 없습니다.")
-    @Length(min = 1, max = 20, message = "title은 1자 이상 20자 이하로 입력해주세요.")
-    String title,
+    @NotEmpty(message = "content는 비어있을 수 없습니다.")
+    @Length(min = 1, max = 20, message = "content는 1자 이상 20자 이하로 입력해주세요.")
+    String content,
 
     @NotNull(message = "repeatDayList는 필수 입력값입니다. (0개도 가능)")
     @Size(max = 7, message = "요일은 7개 이하로 입력해주세요.")
