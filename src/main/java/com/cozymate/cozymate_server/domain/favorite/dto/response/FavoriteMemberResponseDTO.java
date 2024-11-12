b559885cbe907730381a5c36587da1d98b1a3130
jpark0506 <peterp1@inha.edu>
2024-11-12 15:40:56 +0900

    [COZY-352] fix: 빌드 오류 수정

--- src/main/java/com/cozymate/cozymate_server/domain/favorite/dto/response/FavoriteMemberResponseDTO.java
+++ src/main/java/com/cozymate/cozymate_server/domain/favorite/dto/response/FavoriteMemberResponseDTO.java
@@ -1,6 +1,6 @@
 package com.cozymate.cozymate_server.domain.favorite.dto.response;
 
-import com.cozymate.cozymate_server.domain.memberstat.dto.MemberStatResponseDTO.MemberStatPreferenceResponseDTO;
+import com.cozymate.cozymate_server.domain.memberstat.dto.response.MemberStatPreferenceResponseDTO;
 import lombok.Builder;
 
 @Builder
