066589ee0a54d53a33a9df648c220c83e1647251
vero <151748893+veronees@users.noreply.github.com>
2024-11-14 14:59:40 +0900

    [COZY-362] test: 알림 보내는 테스트용 api 추가 (#165)
    * [COZY-362] feat : 알림 보내기 테스트 api 임시 추가
    
    * [COZY-362] fix : fcm 토큰 업데이트 시 valid값도 true로 업데이트 되도록 수정

--- src/main/java/com/cozymate/cozymate_server/domain/fcm/controller/FcmController.java
+++ src/main/java/com/cozymate/cozymate_server/domain/fcm/controller/FcmController.java
@@ -1,14 +1,20 @@
 package com.cozymate.cozymate_server.domain.fcm.controller;
 
 import com.cozymate.cozymate_server.domain.auth.userdetails.MemberDetails;
+import com.cozymate.cozymate_server.domain.fcm.dto.FcmPushTargetDto.OneTargetDto;
 import com.cozymate.cozymate_server.domain.fcm.dto.request.FcmRequestDTO;
 import com.cozymate.cozymate_server.domain.fcm.service.FcmCommandService;
+import com.cozymate.cozymate_server.domain.fcm.service.FcmPushService;
+import com.cozymate.cozymate_server.domain.member.Member;
+import com.cozymate.cozymate_server.domain.member.repository.MemberRepository;
+import com.cozymate.cozymate_server.domain.notificationlog.enums.NotificationType;
 import com.cozymate.cozymate_server.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +34,18 @@ public class FcmController {
     FcmRequestDTO fcmRequestDTO) {
         fcmCommandService.createFcm(memberDetails.member(), fcmRequestDTO);
         return ResponseEntity.ok(ApiResponse.onSuccess("fcm토큰 및 기기 고유 값 저장 완료"));
+    }
+
+
+    private final MemberRepository memberRepository;
+    private final FcmPushService fcmPushService;
+
+    @GetMapping("/test/send")
+    public ResponseEntity<ApiResponse<String>> sendTest() {
+        Member member = memberRepository.findById(7L).get();
+
+        fcmPushService.sendNotification(OneTargetDto.create(member, NotificationType.SELECT_COZY_MATE));
+
+        return ResponseEntity.ok(ApiResponse.onSuccess(member.getNickname() + "에게 알림 전송"));
     }
 }
