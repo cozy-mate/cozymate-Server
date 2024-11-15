5f1d389ffd3dde64227bb757be8f82b37803ead6
veronees <jsg6854@naver.com>
2024-11-16 03:17:04 +0900

    [COZY-369] fix : 찜하기 예외 처리 추가

--- src/main/java/com/cozymate/cozymate_server/domain/favorite/service/FavoriteCommandService.java
+++ src/main/java/com/cozymate/cozymate_server/domain/favorite/service/FavoriteCommandService.java
@@ -6,9 +6,13 @@ import com.cozymate.cozymate_server.domain.favorite.enums.FavoriteType;
 import com.cozymate.cozymate_server.domain.favorite.repository.FavoriteRepository;
 import com.cozymate.cozymate_server.domain.member.Member;
 import com.cozymate.cozymate_server.domain.member.repository.MemberRepository;
+import com.cozymate.cozymate_server.domain.room.Room;
+import com.cozymate.cozymate_server.domain.room.enums.RoomStatus;
+import com.cozymate.cozymate_server.domain.room.enums.RoomType;
 import com.cozymate.cozymate_server.domain.room.repository.RoomRepository;
 import com.cozymate.cozymate_server.global.response.code.status.ErrorStatus;
 import com.cozymate.cozymate_server.global.response.exception.GeneralException;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,14 +31,14 @@ public class FavoriteCommandService {
             throw new GeneralException(ErrorStatus._FAVORITE_CANNOT_REQUEST_SELF);
         }
 
-        checkTargetExists(FavoriteType.MEMBER, favoriteMemberId);
+        validTarget(FavoriteType.MEMBER, favoriteMemberId);
         checkDuplicateFavorite(member, favoriteMemberId, FavoriteType.MEMBER);
 
         favoriteRepository.save(FavoriteConverter.toEntity(member, favoriteMemberId, FavoriteType.MEMBER));
     }
 
     public void saveRoomFavorite(Member member, Long roomId) {
-        checkTargetExists(FavoriteType.ROOM, roomId);
+        validTarget(FavoriteType.ROOM, roomId);
         checkDuplicateFavorite(member, roomId, FavoriteType.ROOM);
 
         favoriteRepository.save(FavoriteConverter.toEntity(member, roomId, FavoriteType.ROOM));
@@ -52,15 +56,31 @@ public class FavoriteCommandService {
         favoriteRepository.delete(favorite);
     }
 
-    private void checkTargetExists(FavoriteType favoriteType, Long targetId) {
+    private void validTarget(FavoriteType favoriteType, Long targetId) {
         if (favoriteType.equals(FavoriteType.MEMBER)) {
-            memberRepository.findById(targetId).orElseThrow(
+            Member member = memberRepository.findById(targetId).orElseThrow(
                 () -> new GeneralException(ErrorStatus._MEMBER_NOT_FOUND)
             );
+
+            if (Objects.isNull(member.getMemberStat())) {
+                throw new GeneralException(ErrorStatus._FAVORITE_CANNOT_MEMBER_WITHOUT_MEMBERSTAT);
+            }
         } else {
-            roomRepository.findById(targetId).orElseThrow(
+            Room room = roomRepository.findById(targetId).orElseThrow(
                 () -> new GeneralException(ErrorStatus._ROOM_NOT_FOUND)
             );
+
+            if (RoomType.PRIVATE.equals(room.getRoomType())) {
+                throw new GeneralException(ErrorStatus._FAVORITE_CANNOT_PRIVATE_ROOM);
+            }
+
+            if (room.getNumOfArrival() == room.getMaxMateNum()) {
+                throw new GeneralException(ErrorStatus._FAVORITE_CANNOT_FULL_ROOM);
+            }
+
+            if (RoomStatus.DISABLE.equals(room.getStatus())) {
+                throw new GeneralException(ErrorStatus._FAVORITE_CANNOT_DISABLE_ROOM);
+            }
         }
     }
 
