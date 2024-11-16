178ff581413c9ef289d0c45c459bae37205146b3
Ga Dong Sik <ret0422@gmail.com>
2024-11-17 01:43:42 +0900

    [COZY-357] feat: 로깅 path 분리

--- src/main/java/com/cozymate/cozymate_server/global/logging/MdcLoggingInterceptor.java
+++ src/main/java/com/cozymate/cozymate_server/global/logging/MdcLoggingInterceptor.java
@@ -3,7 +3,11 @@ package com.cozymate.cozymate_server.global.logging;
 import com.cozymate.cozymate_server.global.logging.enums.MdcKey;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.LocalDateTime;
 import org.slf4j.MDC;
@@ -18,12 +22,13 @@ public class MdcLoggingInterceptor implements HandlerInterceptor {
 
         setMdc(request);
 
-        log.info("[ REQUEST] rid {} | ip {} | method {} | uri {} | param {}",
+        log.info("[ REQUEST] rid {} | ip {} | method {} | uri {} | param {} | path {}",
             MDC.get(MdcKey.REQUEST_ID.name()),
             MDC.get(MdcKey.REQUEST_IP.name()),
             MDC.get(MdcKey.REQUEST_METHOD.name()),
             MDC.get(MdcKey.REQUEST_URI.name()),
-            MDC.get(MdcKey.REQUEST_PARAMS.name())
+            MDC.get(MdcKey.REQUEST_PARAMS.name()),
+            extractPathVariables(request.getRequestURI())
         );
 
         return true;
@@ -33,7 +38,8 @@ public class MdcLoggingInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
         Object handler, Exception ex) throws Exception {
 
-        if (MDC.get(MdcKey.START_TIME_MILLIS.name()) != null && MDC.get(MdcKey.REQUEST_ID.name()) != null) {
+        if (MDC.get(MdcKey.START_TIME_MILLIS.name()) != null
+            && MDC.get(MdcKey.REQUEST_ID.name()) != null) {
             long startTime = Long.parseLong(MDC.get(MdcKey.START_TIME_MILLIS.name()));
             long endTime = System.currentTimeMillis();
             log.info(
@@ -56,7 +62,7 @@ public class MdcLoggingInterceptor implements HandlerInterceptor {
         MDC.put(MdcKey.REQUEST_ID.name(), UUID.randomUUID().toString());
         MDC.put(MdcKey.REQUEST_IP.name(), request.getRemoteAddr());
         MDC.put(MdcKey.REQUEST_METHOD.name(), request.getMethod());
-        MDC.put(MdcKey.REQUEST_URI.name(), request.getRequestURI());
+        MDC.put(MdcKey.REQUEST_URI.name(), request.getRequestURI().replaceAll("\\d+", "{id}"));
         MDC.put(MdcKey.REQUEST_PARAMS.name(), request.getQueryString());
         MDC.put(MdcKey.REQUEST_TIME.name(), LocalDateTime.now().toString());
         MDC.put(MdcKey.START_TIME_MILLIS.name(), String.valueOf(System.currentTimeMillis()));
@@ -71,7 +77,8 @@ public class MdcLoggingInterceptor implements HandlerInterceptor {
             if (agent.contains("mobile")) { // 모바일 여부 확인
                 if (agent.contains("android")) {
                     return "android";
-                } else if (agent.contains("iphone") || agent.contains("ipad") || agent.contains("ios")) {
+                } else if (agent.contains("iphone") || agent.contains("ipad") || agent.contains(
+                    "ios")) {
                     return "ios";
                 } else {
                     return "unknown-mobile";
@@ -82,6 +89,18 @@ public class MdcLoggingInterceptor implements HandlerInterceptor {
         } else {
             return agent; // User-Agent가 없는 경우
         }
+    }
 
+    private String extractPathVariables(String requestUri) {
+        // 정규식으로 숫자 패턴 추출
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(requestUri);
+
+        List<String> pathVariables = new ArrayList<>();
+        while (matcher.find()) {
+            pathVariables.add(matcher.group());
+        }
+
+        return pathVariables.toString();
     }
 }
