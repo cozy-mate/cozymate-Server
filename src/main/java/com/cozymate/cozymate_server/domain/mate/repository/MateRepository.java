c0c448770e8234f9affb769882a06bb828ce1750
Ga Dong Sik <62535229+eple0329@users.noreply.github.com>
2024-11-21 01:42:16 +0900

    [COZY-391] refac: 방 추천 기능 개선 (#181)
    * [COZY-391] feat: 방 추천 로직 수정
    
    * [COZY-391] feat: 응답값 추가
    
    * [COZY-391] fix: size값 출력 오류 수정
    
    * [COZY-391] docs: swagger 코드 해결
    
    * [COZY-391] fix: equals 에러 해결
    
    ---------
    
    Co-authored-by: jpark0506 <peterp1@inha.edu>

--- src/main/java/com/cozymate/cozymate_server/domain/mate/repository/MateRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/mate/repository/MateRepository.java
@@ -82,8 +82,6 @@ public interface MateRepository extends JpaRepository<Mate, Long> {
     @Query("select m.id from Mate m where m.member.id in :memberIds")
     Set<Long> findMateIdsByMemberIds(@Param("memberIds") Set<Long> memberIds);
 
-    List<Mate> findByIdIn(List<Long> memberIdList);
-
     void deleteByRoomIdAndMemberId(Long roomId, Long memberId);
 
     List<Mate> findByRoomIdAndEntryStatus(Long roomId, EntryStatus entryStatus);
@@ -93,4 +91,6 @@ public interface MateRepository extends JpaRepository<Mate, Long> {
     @Query("select m from Mate m join fetch m.member where m.room = :room and m.isRoomManager = :isRoomManager")
     Optional<Mate> findFetchByRoomAndIsRoomManager(@Param("room") Room room,
         @Param("isRoomManager") boolean isRoomManager);
+
+    List<Mate> findAllByEntryStatus(EntryStatus entryStatus);
 }
--- src/main/java/com/cozymate/cozymate_server/domain/room/repository/RoomRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/room/repository/RoomRepository.java
@@ -12,13 +12,26 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
     boolean existsByName(String roomName);
+
     boolean existsByInviteCode(String inviteCode);
+
     @Query("SELECT COUNT(m) > 0 FROM Mate m WHERE m.member.id = :memberId AND (m.room.status = :status1 OR m.room.status = :status2) AND m.entryStatus = :status3")
-    boolean existsByMemberIdAndStatuses(@Param("memberId") Long memberId, @Param("status1") RoomStatus status1, @Param("status2") RoomStatus status2, @Param("status3") EntryStatus status3);
+    boolean existsByMemberIdAndStatuses(@Param("memberId") Long memberId,
+        @Param("status1") RoomStatus status1, @Param("status2") RoomStatus status2,
+        @Param("status3") EntryStatus status3);
+
     Optional<Room> findByInviteCode(String inviteCode);
 
-    List<Room> findAllByRoomTypeAndStatusNot(RoomType roomType, RoomStatus status);
+    @Query("""
+        SELECT r FROM Room r
+        WHERE r.roomType = :roomType
+        AND r.status != :status
+        AND r.maxMateNum > r.numOfArrival
+        """)
+    List<Room> findAllRoomListCanDisplay(@Param("roomType") RoomType roomType,
+        @Param("status") RoomStatus status);
 
     @Query("SELECT r FROM Room r " +
         "JOIN FETCH Mate m ON r.id = m.room.id AND m.isRoomManager = true " +
