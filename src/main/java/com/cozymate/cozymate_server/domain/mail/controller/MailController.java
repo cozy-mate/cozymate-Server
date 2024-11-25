20dbd298176044bf632c43d2c45b2e83cf1b6fb6
Ga Dong Sik <ret0422@gmail.com>
2024-11-25 14:56:38 +0900

    [COZY-397] feat: 메일 인증 여부 반환하는 API 구현

--- src/main/java/com/cozymate/cozymate_server/domain/mail/controller/MailController.java
+++ src/main/java/com/cozymate/cozymate_server/domain/mail/controller/MailController.java
@@ -7,7 +7,6 @@ import com.cozymate.cozymate_server.domain.mail.dto.request.VerifyRequestDTO;
 import com.cozymate.cozymate_server.domain.mail.dto.response.VerifyResponseDTO;
 import com.cozymate.cozymate_server.domain.mail.service.MailService;
 import com.cozymate.cozymate_server.global.response.ApiResponse;
-
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,16 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/members/mail")
 public class MailController {
+
     private final MailService mailService;
 
     @PostMapping("")
     @Operation(summary = "[말즈] 메일 보내기 API",
-            description = "request body :  <br>"
-                    + "mailAddress : 12345@inha.edu <br>"
-                    + "universityId : 1 <br>")
+        description = "request body :  <br>"
+            + "mailAddress : 12345@inha.edu <br>"
+            + "universityId : 1 <br>")
     public ResponseEntity<Void> sendUniversityAuthenticationCode(
-            @AuthenticationPrincipal MemberDetails memberDetails,
-            @RequestBody MailSendRequestDTO sendDTO
+        @AuthenticationPrincipal MemberDetails memberDetails,
+        @RequestBody MailSendRequestDTO sendDTO
     ) {
         mailService.sendUniversityAuthenticationCode(memberDetails, sendDTO);
         return ResponseEntity.ok().build();
@@ -39,17 +39,27 @@ public class MailController {
 
     @PostMapping("/verify")
     @Operation(summary = "[말즈] 메일 인증 API",
-            description = "request body :  <br>"
-                    + " code : a1B2c3 <br>"
-                    + " universityId : 1 <br>"
-                    + "response :"
-                    + " 토큰")
+        description = "request body :  <br>"
+            + " code : a1B2c3 <br>"
+            + " universityId : 1 <br>"
+            + "response :"
+            + " 토큰")
     public ResponseEntity<ApiResponse<VerifyResponseDTO>> verifyMemberUniversity(
-            @AuthenticationPrincipal MemberDetails memberDetails,
-            @RequestBody VerifyRequestDTO verifyDTO
+        @AuthenticationPrincipal MemberDetails memberDetails,
+        @RequestBody VerifyRequestDTO verifyDTO
 
-            ) {
-        return ResponseEntity.ok(ApiResponse.onSuccess(mailService.verifyMemberUniversity(memberDetails,verifyDTO)));
+    ) {
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(mailService.verifyMemberUniversity(memberDetails, verifyDTO)));
+    }
+
+    @GetMapping("/verify")
+    @Operation(summary = "[무빗] 메일 인증 여부 반환", description = "메일인증을 받은적이 없거나, 받았는데 인증 확인이 안된 경우 false 반환, 메일 인증을 받고, 인증 확인이 된 경우 true 반환")
+    public ResponseEntity<ApiResponse<Boolean>> isVerified(
+        @AuthenticationPrincipal MemberDetails memberDetails
+    ) {
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(mailService.isVerified(memberDetails.member())));
     }
 
 }
--- src/main/java/com/cozymate/cozymate_server/domain/mail/service/MailService.java
+++ src/main/java/com/cozymate/cozymate_server/domain/mail/service/MailService.java
@@ -22,8 +22,8 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -31,50 +31,60 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Slf4j
 public class MailService {
-    private final JavaMailSender mailSender;
-    private final MailRepository mailRepository;
-
-    private final MemberCommandService memberCommandService;
-
-    private final UniversityRepository universityRepository;
 
     private static final Integer MAIL_AUTHENTICATION_EXPIRED_TIME = 30;
+    private final JavaMailSender mailSender;
+    private final MailRepository mailRepository;
+    private final MemberCommandService memberCommandService;
+    private final UniversityRepository universityRepository;
 
     @Transactional
-    public void sendUniversityAuthenticationCode(MemberDetails memberDetails, MailSendRequestDTO sendDTO) {
+    public void sendUniversityAuthenticationCode(MemberDetails memberDetails,
+        MailSendRequestDTO sendDTO) {
         University university = universityRepository.findById(sendDTO.universityId())
-                .orElseThrow(() -> new GeneralException(ErrorStatus._UNIVERSITY_NOT_FOUND));
+            .orElseThrow(() -> new GeneralException(ErrorStatus._UNIVERSITY_NOT_FOUND));
 
         String mailAddress = sendDTO.mailAddress();
         validateMailAddress(mailAddress, university.getMailPattern());
 
         MailAuthentication mailAuthentication = createAndSendMail(memberDetails.member().getId(),
-                mailAddress);
+            mailAddress);
 
         mailRepository.save(mailAuthentication);
     }
 
     @Transactional
     public VerifyResponseDTO verifyMemberUniversity(MemberDetails memberDetails,
-                                                    VerifyRequestDTO verifyDTO) {
+        VerifyRequestDTO verifyDTO) {
         Member member = memberDetails.member();
 
         verifyAuthenticationCode(member, verifyDTO.code());
 
-        TokenResponseDTO tokenResponseDTO = memberCommandService.verifyMemberUniversity(memberDetails,
-                verifyDTO.universityId(),
-                verifyDTO.majorName());
+        TokenResponseDTO tokenResponseDTO = memberCommandService.verifyMemberUniversity(
+            memberDetails,
+            verifyDTO.universityId(),
+            verifyDTO.majorName());
         return MailConverter.toVerifyResponseDTO(tokenResponseDTO);
+    }
+
+    public Boolean isVerified(Member member) {
+        Optional<MailAuthentication> mailAuthentication = mailRepository.findById(member.getId());
+
+        if (mailAuthentication.isPresent() && mailAuthentication.get().getIsVerified()) {
+            return true;
+        }
+        return false;
     }
 
     private void verifyAuthenticationCode(Member member, String requestCode) {
 
         MailAuthentication mailAuthentication = mailRepository.findById(member.getId())
-                .orElseThrow(() -> new GeneralException(
-                        ErrorStatus._MAIL_AUTHENTICATION_NOT_FOUND));
+            .orElseThrow(() -> new GeneralException(
+                ErrorStatus._MAIL_AUTHENTICATION_NOT_FOUND));
         // 만료 시간 초과 여부 확인
         if (LocalDateTime.now()
-                .isAfter(mailAuthentication.getUpdatedAt().plusMinutes(MAIL_AUTHENTICATION_EXPIRED_TIME))) {
+            .isAfter(
+                mailAuthentication.getUpdatedAt().plusMinutes(MAIL_AUTHENTICATION_EXPIRED_TIME))) {
             throw new GeneralException(ErrorStatus._MAIL_AUTHENTICATION_CODE_EXPIRED);
         }
 
@@ -89,19 +99,22 @@ public class MailService {
     private MailAuthentication createAndSendMail(Long memberId, String mailAddress) {
         SimpleMailMessage message = new SimpleMailMessage();
 
-        String authenticationCode = Base64.getEncoder().encodeToString(UUID.randomUUID().toString().getBytes())
-                .substring(0, 6);
+        String authenticationCode = Base64.getEncoder()
+            .encodeToString(UUID.randomUUID().toString().getBytes())
+            .substring(0, 6);
 
         message.setTo(mailAddress);
         message.setSubject("COZYMATE 대학교 메일인증");
         message.setText("COZYMATE 대학교 메일인증 코드입니다 : " + authenticationCode);
         mailSender.send(message);
 
-        return MailConverter.toMailAuthenticationWithParams(memberId, mailAddress, authenticationCode, false);
+        return MailConverter.toMailAuthenticationWithParams(memberId, mailAddress,
+            authenticationCode, false);
     }
 
     private void validateMailAddress(String mailAddress, String mailPattern) {
-        Optional<MailAuthentication> mailAuthentication = mailRepository.findByMailAddress(mailAddress);
+        Optional<MailAuthentication> mailAuthentication = mailRepository.findByMailAddress(
+            mailAddress);
 
         if (!mailAddress.contains(mailPattern)) {
             throw new GeneralException(ErrorStatus._INVALID_MAIL_ADDRESS_DOMAIN);
