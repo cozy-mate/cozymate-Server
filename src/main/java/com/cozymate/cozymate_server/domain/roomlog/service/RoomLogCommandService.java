1ae26678b5c73d6a094483104ba933207b0fdbf5
Ga Dong Sik <ret0422@gmail.com>
2024-11-30 23:36:32 +0900

    [COZY-408] refac: 불필요한 import 제거

--- src/main/java/com/cozymate/cozymate_server/domain/roomlog/service/RoomLogCommandService.java
+++ src/main/java/com/cozymate/cozymate_server/domain/roomlog/service/RoomLogCommandService.java
@@ -16,7 +16,6 @@ import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
