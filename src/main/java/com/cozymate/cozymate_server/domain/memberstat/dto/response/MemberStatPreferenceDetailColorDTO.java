8316afc86c24d056dde181811a162d87e617769b
jpark0506 <peterp1@inha.edu>
2024-12-01 01:32:00 +0900

    [COZY-410] feat: 주석 달기

--- src/main/java/com/cozymate/cozymate_server/domain/memberstat/dto/response/MemberStatPreferenceDetailColorDTO.java
+++ src/main/java/com/cozymate/cozymate_server/domain/memberstat/dto/response/MemberStatPreferenceDetailColorDTO.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 @Builder
 public record MemberStatPreferenceDetailColorDTO(
     String stat,
+    // 다양한 Stat 값이 들어갈 수 있어 Object로 정의.
     Object value,
     String color
 ) {
