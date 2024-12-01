b3b1ea387e322554418c03620994539b68ac48f8
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-12-01 23:01:44 +0900

    [COZY-411] feat : enum값 관련 interface 생성

--- src/main/java/com/cozymate/cozymate_server/domain/auth/utils/ClientIdMaker.java
+++ src/main/java/com/cozymate/cozymate_server/domain/auth/utils/ClientIdMaker.java
@@ -10,7 +10,7 @@ import java.util.UUID;
  * 소셜로그인 타입과, 소셜 서비스에서 제공한 clientId로 자체 클라이언트 id 를 만드는 util 클래스
  * ex.
  * 카카오 id가 12345, 소셜 타입이 KAKAO 면
- * -> clientId = "12345@KAKAO"
+ * -> clientId = "12345:KAKAO"
  */
 public class ClientIdMaker {
     public static final int MEMBER_ID_INDEX = 0;
@@ -28,7 +28,7 @@ public class ClientIdMaker {
     }
     public static SocialType getSocialTypeInClientId(String clientId) {
         String socialTypePart = Arrays.asList(clientId.split(DELIMITER)).get(SOCIAL_TYPE_INDEX);
-        return SocialType.valueOf(socialTypePart);
+        return SocialType.getValue(socialTypePart);
     }
 
     public static String getClientIdAtSocialService(String clientId) {
