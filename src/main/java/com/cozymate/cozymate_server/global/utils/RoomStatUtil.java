f138617b6f1dab7e938a72eccea9425484a9c482
veronees <jsg6854@naver.com>
2024-12-05 22:04:47 +0900

    [COZY-445] refac: 방 일치율, 칩 일치수를 계산하는 메서드를 유틸 클래스로 분리한다

---
+++ src/main/java/com/cozymate/cozymate_server/global/utils/RoomStatUtil.java
@@ -0,0 +1,58 @@
+package com.cozymate.cozymate_server.global.utils;
+
+import com.cozymate.cozymate_server.domain.favorite.dto.response.PreferenceMatchCountDTO;
+import com.cozymate.cozymate_server.domain.mate.Mate;
+import com.cozymate.cozymate_server.domain.member.Member;
+import com.cozymate.cozymate_server.domain.memberstat.MemberStat;
+import com.cozymate.cozymate_server.domain.memberstat.util.MemberStatUtil;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RoomStatUtil {
+
+    public static List<PreferenceMatchCountDTO> getPreferenceStatsMatchCounts(Member member,
+        List<String> criteriaPreferenceList, List<Mate> mates, MemberStat memberStat) {
+
+        List<PreferenceMatchCountDTO> preferenceMatchCountDTOList = criteriaPreferenceList.stream()
+            .map(preference -> {
+                long equalCount = mates.stream()
+                    .map(mate -> mate.getMember().getMemberStat())
+                    .filter(mateStat -> mateStat != null && !mateStat.getMember().getId().equals(
+                        member.getId())
+                        && Objects.equals(MemberStatUtil.getMemberStatField(mateStat, preference),
+                        MemberStatUtil.getMemberStatField(memberStat, preference)))
+                    .count();
+
+                return PreferenceMatchCountDTO.builder()
+                    .preferenceName(preference)
+                    .count((int) equalCount)
+                    .build();
+            }).toList();
+
+        return preferenceMatchCountDTOList;
+    }
+
+    public static List<PreferenceMatchCountDTO> getPreferenceStatsMatchCountsWithoutMemberStat(
+        List<String> criteriaPreferenceList) {
+        return criteriaPreferenceList.stream()
+            .map(preference -> {
+                return PreferenceMatchCountDTO.builder()
+                    .preferenceName(preference)
+                    .count(null)
+                    .build();
+            }).toList();
+    }
+
+    public static Integer getCalculateRoomEquality(Map<Long, Integer> equalityMap){
+        List<Integer> roomEquality = equalityMap.values().stream()
+            .toList();
+        int sum = roomEquality.stream().mapToInt(Integer::intValue).sum();
+        return roomEquality.isEmpty() ? null : sum / roomEquality.size();
+
+    }
+}
