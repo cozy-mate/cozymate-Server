0e9ced1b83b1ed8d10771032470bb35c643f20c2
김수환 <101265039+genius00hwan@users.noreply.github.com>
2024-12-28 23:42:11 +0900

    [COZY-454]  feat : 토큰 위치 레디스로 수정 (#219)
    * [COZY-454] feat : 토큰 위치 레디스로 수정
    
    * [COZY-454] add : 토큰 레포지토리 레디스로 추가
    
    * [COZY-454] fix : 리프레시 토큰 검증 함수 이름 수정

--- src/main/java/com/cozymate/cozymate_server/domain/auth/repository/TokenRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/auth/repository/TokenRepository.java
@@ -2,8 +2,8 @@ package com.cozymate.cozymate_server.domain.auth.repository;
 
 import com.cozymate.cozymate_server.domain.auth.Token;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TokenRepository extends JpaRepository<Token, String> {
-    Optional<Token> findByRefreshToken(String refreshTokenValue);
+public interface TokenRepository extends CrudRepository<Token, String> {
+    Optional<Token> findByRefreshToken(String refreshToken);
 }
