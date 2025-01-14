cca07081e24f6cb489bf8d958c7571cc27112a0e
veronees <jsg6854@naver.com>
2025-01-14 19:15:49 +0900

    [COZY-476] fix: ChatRoom 테스트 코드 주석을 해제한다

--- src/test/java/com/cozymate/cozymate_server/domain/chatroom/ChatRoomTestBuilder.java
+++ src/test/java/com/cozymate/cozymate_server/domain/chatroom/ChatRoomTestBuilder.java
@@ -1,29 +1,29 @@
-//package com.cozymate.cozymate_server.domain.chatroom;
-//
-//import com.cozymate.cozymate_server.domain.chat.ChatTestBuilder;
-//import com.cozymate.cozymate_server.domain.member.Member;
-//
-//public class ChatRoomTestBuilder {
-//
-//    public static ChatRoom testChatRoomBuild() {
-//        Member sender = ChatTestBuilder.testSenderBuild();
-//        Member recipient = ChatTestBuilder.testRecipientBuild();
-//
-//        return ChatRoom.builder()
-//            .id(1L)
-//            .memberA(sender)
-//            .memberB(recipient)
-//            .build();
-//    }
-//
-//    public static ChatRoom testChat2RoomBuild() {
-//        Member sender = ChatTestBuilder.testSenderBuild();
-//        Member otherMember = ChatTestBuilder.testOtherMemberBuild();
-//
-//        return ChatRoom.builder()
-//            .id(2L)
-//            .memberA(sender)
-//            .memberB(otherMember)
-//            .build();
-//    }
-//}
+package com.cozymate.cozymate_server.domain.chatroom;
+
+import com.cozymate.cozymate_server.domain.chat.ChatTestBuilder;
+import com.cozymate.cozymate_server.domain.member.Member;
+
+public class ChatRoomTestBuilder {
+
+    public static ChatRoom testChatRoomBuild() {
+        Member sender = ChatTestBuilder.testSenderBuild();
+        Member recipient = ChatTestBuilder.testRecipientBuild();
+
+        return ChatRoom.builder()
+            .id(1L)
+            .memberA(sender)
+            .memberB(recipient)
+            .build();
+    }
+
+    public static ChatRoom testChat2RoomBuild() {
+        Member sender = ChatTestBuilder.testSenderBuild();
+        Member otherMember = ChatTestBuilder.testOtherMemberBuild();
+
+        return ChatRoom.builder()
+            .id(2L)
+            .memberA(sender)
+            .memberB(otherMember)
+            .build();
+    }
+}
