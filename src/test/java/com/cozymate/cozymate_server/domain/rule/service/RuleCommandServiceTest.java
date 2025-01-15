764e659d0aae200edfd986644b01df7221cf9d1f
Ga Dong Sik <ret0422@gmail.com>
2025-01-15 15:34:20 +0900

    [COZY-475] test: Rule 생성 테스트코드 임시 작성

---
+++ src/test/java/com/cozymate/cozymate_server/domain/rule/service/RuleCommandServiceTest.java
@@ -0,0 +1,116 @@
+package com.cozymate.cozymate_server.domain.rule.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import com.cozymate.cozymate_server.domain.mate.Mate;
+import com.cozymate.cozymate_server.domain.mate.enums.EntryStatus;
+import com.cozymate.cozymate_server.domain.mate.repository.MateRepository;
+import com.cozymate.cozymate_server.domain.member.Member;
+import com.cozymate.cozymate_server.domain.member.enums.Gender;
+import com.cozymate.cozymate_server.domain.member.enums.Role;
+import com.cozymate.cozymate_server.domain.member.enums.SocialType;
+import com.cozymate.cozymate_server.domain.room.Room;
+import com.cozymate.cozymate_server.domain.rule.Rule;
+import com.cozymate.cozymate_server.domain.rule.dto.request.CreateRuleRequestDTO;
+import com.cozymate.cozymate_server.domain.rule.dto.response.RuleIdResponseDTO;
+import com.cozymate.cozymate_server.domain.rule.repository.RuleRepository;
+import com.cozymate.cozymate_server.fixture.RuleFixture;
+import com.cozymate.cozymate_server.global.response.exception.GeneralException;
+import java.time.LocalDate;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@SuppressWarnings("NonAsciiCharacters")
+@ExtendWith(MockitoExtension.class)
+public class RuleCommandServiceTest {
+
+    private static final RuleFixture ruleFixture = new RuleFixture();
+
+    @Mock
+    private RuleRepository ruleRepository;
+    @Mock
+    private MateRepository mateRepository;
+
+    @InjectMocks
+    private RuleCommandService ruleCommandService;
+
+    @Nested
+    class Rule_규칙_생성 {
+
+        private Room room;
+        private Member member;
+        private Mate mate;
+        private CreateRuleRequestDTO requestDto;
+        private Rule rule;
+
+        @BeforeEach
+        void setUp() {
+            // given : Rule 테스트 전체에서 공통으로 쓰이는 데이터 준비
+            room = Room.builder().id(1L).build(); // TODO: 기본 방 -> Fixture로 대체
+            member = Member.builder().id(1L).socialType(SocialType.KAKAO).role(Role.USER_VERIFIED)
+                .clientId("qwer:KAKAO").nickname("무빗").gender(Gender.MALE).birthDay(LocalDate.MAX)
+                .persona(1).build(); // TODO: 기본 멤버 -> Fixture로 대체
+            mate = Mate.builder().id(1L).room(room).member(member)
+                .entryStatus(EntryStatus.JOINED).build(); // TODO: 기본 방 멤버 -> Fixture로 대체
+
+            when(mateRepository.findByRoomIdAndMemberIdAndEntryStatus(room.getId(), member.getId(),
+                EntryStatus.JOINED)).thenReturn(Optional.ofNullable(mate)); // 방 멤버 조회
+
+            rule = ruleFixture.규칙_1(room); // 생성되었을 때 사용할 규칙
+            requestDto = ruleFixture.규칙_1_생성_요청_DTO(); // 기본 규칙 생성 요청 DTO
+            lenient().when(ruleRepository.save(any()))
+                .thenReturn(rule); // 규칙 생성
+        }
+
+        @Test
+        void 기존규칙이_없을때_성공한다() {
+            // given
+            when(ruleRepository.countAllByRoomId(room.getId())).thenReturn(0); // 기존에 규칙이 없는 경우
+
+            // when
+            RuleIdResponseDTO responseDto = ruleCommandService.createRule(member, room.getId(),
+                requestDto);
+
+            // then
+            assertThat(responseDto.ruleId()).isEqualTo(ruleFixture.규칙_1(room).getId());
+
+        }
+
+        @Test
+        void 기존규칙이_9개일때_성공한다() {
+            // given
+            when(ruleRepository.countAllByRoomId(room.getId())).thenReturn(9); // 기존에 규칙이 9개인 경우
+
+            // when
+            RuleIdResponseDTO responseDto = ruleCommandService.createRule(member, room.getId(),
+                requestDto);
+
+            // then
+            assertThat(responseDto.ruleId()).isEqualTo(ruleFixture.규칙_1(room).getId());
+
+        }
+
+        @Test
+        void 기존규칙이_10개일때_실패한다() {
+            // given
+            when(ruleRepository.countAllByRoomId(room.getId())).thenReturn(
+                10); // 기존에 규칙이 10개인 경우
+
+            // when, then
+            assertThatThrownBy(
+                () -> ruleCommandService.createRule(member, room.getId(), requestDto))
+                .isInstanceOf(GeneralException.class);
+        }
+    }
+}
--- src/test/java/com/cozymate/cozymate_server/fixture/RuleFixture.java
+++ src/test/java/com/cozymate/cozymate_server/fixture/RuleFixture.java
@@ -2,12 +2,13 @@ package com.cozymate.cozymate_server.fixture;
 
 import com.cozymate.cozymate_server.domain.room.Room;
 import com.cozymate.cozymate_server.domain.rule.Rule;
-import jakarta.annotation.PostConstruct;
+import com.cozymate.cozymate_server.domain.rule.dto.request.CreateRuleRequestDTO;
+import java.util.List;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class RuleFixture {
 
-    public Rule 규칙_1 (Room room) {
+    public Rule 규칙_1(Room room) {
         return Rule.builder()
             .room(room)
             .content("20:00-04:00 사이 코어타임 들어오기")
@@ -15,7 +16,7 @@ public class RuleFixture {
             .build();
     }
 
-    public Rule 규칙_2 (Room room) {
+    public Rule 규칙_2(Room room) {
         return Rule.builder()
             .room(room)
             .content("친구를 데려오지말자")
@@ -23,7 +24,7 @@ public class RuleFixture {
             .build();
     }
 
-    public Rule 규칙_3 (Room room) {
+    public Rule 규칙_3(Room room) {
         return Rule.builder()
             .room(room)
             .content("배달음식은 모두의 동의를 받고 같이 먹기")
@@ -31,7 +32,7 @@ public class RuleFixture {
             .build();
     }
 
-    public Rule 규칙_4 (Room room) {
+    public Rule 규칙_4(Room room) {
         return Rule.builder()
             .room(room)
             .content("배달음식은 모두의 동의를 받고 같이 먹기")
@@ -39,7 +40,7 @@ public class RuleFixture {
             .build();
     }
 
-    public Rule 메모가_null인_규칙 (Room room) {
+    public Rule 메모가_null인_규칙(Room room) {
         return Rule.builder()
             .room(room)
             .content("배달음식은 모두의 동의를 받고 같이 먹기")
@@ -47,7 +48,7 @@ public class RuleFixture {
             .build();
     }
 
-    public Rule 메모가_너무_많은_규칙 (Room room) {
+    public Rule 메모가_너무_많은_규칙(Room room) {
         return Rule.builder()
             .room(room)
             .content("메모의 최대 길이를 체크해보는 중입니다.")
@@ -55,19 +56,79 @@ public class RuleFixture {
             .build();
     }
 
-    public Rule 컨텐츠가_너무_많은_규칙 (Room room) {
+    public Rule 컨텐츠가_너무_많은_규칙(Room room) {
         return Rule.builder()
             .room(room)
-            .content("컨텐츠의 최대 길이도 50자라고 하는데요. 이걸 제가 다 채우기 위해서는 수많은 노력이 필요하고, 지금 저는 그걸 다 끝내고 있죠.") // 73자
+            .content(
+                "컨텐츠의 최대 길이도 50자라고 하는데요. 이걸 제가 다 채우기 위해서는 수많은 노력이 필요하고, 지금 저는 그걸 다 끝내고 있죠.") // 73자
             .memo("이제는 컨텐츠의 최대 길이를 체크하고 있습니다.")
             .build();
     }
 
-    public Rule 방이_없는_규칙 () {
+    public Rule 방이_없는_규칙() {
         return Rule.builder()
             .room(null)
-            .content("컨텐츠의 최대 길이도 50자라고 하는데요. 이걸 제가 다 채우기 위해서는 수많은 노력이 필요하고, 지금 저는 그걸 다 끝내고 있죠.") // 73자
+            .content(
+                "컨텐츠의 최대 길이도 50자라고 하는데요. 이걸 제가 다 채우기 위해서는 수많은 노력이 필요하고, 지금 저는 그걸 다 끝내고 있죠.") // 73자
             .memo("이제는 컨텐츠의 최대 길이를 체크하고 있습니다.")
             .build();
+    }
+
+    public CreateRuleRequestDTO 규칙_1_생성_요청_DTO() {
+        return CreateRuleRequestDTO.builder()
+            .content("20:00-04:00 사이 코어타임 들어오기")
+            .memo("안들어오면 델로랑 싸움")
+            .build();
+    }
+
+    public CreateRuleRequestDTO 규칙_2_생성_요청_DTO() {
+        return CreateRuleRequestDTO.builder()
+            .content("친구를 데려오지말자")
+            .memo("데려올거면 미리 허락받기")
+            .build();
+    }
+
+    public CreateRuleRequestDTO 규칙_3_생성_요청_DTO() {
+        return CreateRuleRequestDTO.builder()
+            .content("배달음식은 모두의 동의를 받고 같이 먹기")
+            .memo("같이 안먹으면 배고프니까")
+            .build();
+    }
+
+    public CreateRuleRequestDTO 규칙_4_생성_요청_DTO() {
+        return CreateRuleRequestDTO.builder()
+            .content("배달음식은 모두의 동의를 받고 같이 먹기")
+            .memo("")
+            .build();
+    }
+
+    public CreateRuleRequestDTO 메모가_null인_규칙_생성_요청_DTO() {
+        return CreateRuleRequestDTO.builder()
+            .content("배달음식은 모두의 동의를 받고 같이 먹기")
+            .memo(null)
+            .build();
+    }
+
+    public CreateRuleRequestDTO 메모가_너무_많은_규칙_생성_요청_DTO() {
+        return CreateRuleRequestDTO.builder()
+            .content("메모의 최대 길이를 체크해보는 중입니다.")
+            .memo("메모의 최대 길이는 50자라고 하네요. 이걸 제가 다 채우기 위해서는 수많은 노력이 필요한데, 이렇게 열심히 쓰려고 해야한답니다.") // 72자
+            .build();
+    }
+
+    public CreateRuleRequestDTO 컨텐츠가_너무_많은_규칙_생성_요청_DTO() {
+        return CreateRuleRequestDTO.builder()
+            .content(
+                "컨텐츠의 최대 길이도 50자라고 하는데요. 이걸 제가 다 채우기 위해서는 수많은 노력이 필요하고, 지금 저는 그걸 다 끝내고 있죠.") // 73자
+            .memo("이제는 컨텐츠의 최대 길이를 체크하고 있습니다.")
+            .build();
+    }
+
+    public List<Rule> 정상_Rule_리스트(Room room) {
+        return List.of(규칙_1(room), 규칙_2(room), 규칙_3(room), 규칙_4(room));
+    }
+
+    public List<CreateRuleRequestDTO> 정상_Rule_생성_요청_DTO_리스트() {
+        return List.of(규칙_1_생성_요청_DTO(), 규칙_2_생성_요청_DTO(), 규칙_3_생성_요청_DTO(), 규칙_4_생성_요청_DTO());
     }
 }
