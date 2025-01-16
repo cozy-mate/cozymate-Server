6c271801d512418ff00791743b293717789085e5
veronees <jsg6854@naver.com>
2025-01-16 17:18:13 +0900

    [COZY-476] test: ChatRoom 정상 더미데이터를 추가한다

--- src/test/java/com/cozymate/cozymate_server/fixture/ChatRoomFixture.java
+++ src/test/java/com/cozymate/cozymate_server/fixture/ChatRoomFixture.java
@@ -6,7 +6,7 @@ import com.cozymate.cozymate_server.domain.member.Member;
 @SuppressWarnings("NonAsciiCharacters")
 public class ChatRoomFixture {
 
-    // 정상 더미데이터
+    // 정상 더미데이터, 멤버 둘다 탈퇴하지 않은 경우
     public ChatRoom 정상_1(Member memberA, Member memberB) {
         return ChatRoom.builder()
             .id(1L)
@@ -15,7 +15,7 @@ public class ChatRoomFixture {
             .build();
     }
 
-    // 정상 더미데이터
+    // 정상 더미데이터, 멤버 둘다 탈퇴하지 않은 경우
     public ChatRoom 정상_2(Member memberA, Member memberB) {
         return ChatRoom.builder()
             .id(2L)
@@ -24,12 +24,21 @@ public class ChatRoomFixture {
             .build();
     }
 
-    // 정상 더미데이터
+    // 정상 더미데이터, 멤버 둘다 탈퇴하지 않은 경우
     public ChatRoom 정상_3(Member memberA, Member memberB) {
         return ChatRoom.builder()
             .id(3L)
             .memberA(memberA)
             .memberB(memberB)
+            .build();
+    }
+
+    // 정상 더미데이터, member 한명이 탈퇴한 경우
+    public ChatRoom 정상_4(Member member) {
+        return ChatRoom.builder()
+            .id(4L)
+            .memberA(member)
+            .memberB(null)
             .build();
     }
 }
