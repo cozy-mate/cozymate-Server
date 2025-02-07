06859942badb801173b6323e6daf8e7495b6417a
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2025-02-08 00:22:48 +0900

    [COZY-508] fix : `deleteAllByMemberId` 벌크연산에 `@Modifying`, `@Transactional` 추가

--- src/main/java/com/cozymate/cozymate_server/domain/memberstat/lifestylematchrate/repository/LifestyleMatchRateRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/memberstat/lifestylematchrate/repository/LifestyleMatchRateRepository.java
@@ -4,8 +4,10 @@ import com.cozymate.cozymate_server.domain.memberstat.lifestylematchrate.Lifesty
 import com.cozymate.cozymate_server.domain.memberstat.lifestylematchrate.LifestyleMatchRate.LifestyleMatchRateId;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface LifestyleMatchRateRepository extends
     JpaRepository<LifestyleMatchRate, LifestyleMatchRateId> {
@@ -16,6 +18,8 @@ public interface LifestyleMatchRateRepository extends
     @Query("SELECT l FROM LifestyleMatchRate l WHERE l.id IN :idList")
     List<LifestyleMatchRate> findByIdList(@Param("idList") List<LifestyleMatchRateId> idList);
 
+    @Modifying
+    @Transactional
     @Query("DELETE FROM LifestyleMatchRate lmr WHERE lmr.id.memberA = :memberId OR lmr.id.memberB = :memberId")
     void deleteAllByMemberId(@Param("memberId") Long memberId);
 
