9515f32af22ed73519f96cfbd701582d0b9f0b7a
subin <118423039+suuu0719@users.noreply.github.com>
2025-03-04 03:50:01 +0900

    Revert "[COZY-527] fix: RoomHashtag 관계 변경 (#281)" (#291)
    This reverts commit fc10b8ede404acb9bb23616ee5e7ff81e08977a2.

--- src/main/java/com/cozymate/cozymate_server/domain/hashtag/repository/HashtagRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/hashtag/repository/HashtagRepository.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
-    Optional<Hashtag> findByName(String name);
+    Optional<Hashtag> findByHashtag(String hashtag);
 
 }
