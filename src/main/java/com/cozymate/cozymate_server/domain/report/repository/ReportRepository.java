bb5e22403ca998167b1653697940babec97dca01
momuzzi <151748893+momuzzi@users.noreply.github.com>
2025-03-18 20:46:54 +0900

    [COZY-549] refac: Inquiry, Report 도메인 Validator, RepositoryService 분리 리팩토링 (#309)
    * [COZY-549] refac: Inquiry에 대해 Validator, RepositoryService 분리 리팩토링을 한다
    
    * [COZY-549] refac: Report에 대해 Validator, RepositoryService 분리 리팩토링을 한다
    
    * [COZY-549] fix: validator 논리 오류를 수정한다

--- src/main/java/com/cozymate/cozymate_server/domain/report/repository/ReportRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/report/repository/ReportRepository.java
@@ -4,9 +4,6 @@ import com.cozymate.cozymate_server.domain.member.Member;
 import com.cozymate.cozymate_server.domain.report.Report;
 import com.cozymate.cozymate_server.domain.report.enums.ReportReason;
 import com.cozymate.cozymate_server.domain.report.enums.ReportSource;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,8 +13,6 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
 
     boolean existsByReporterAndReportedMemberIdAndReportReasonAndReportSource(Member reporter,
         Long reportedMemberId, ReportReason reportReason, ReportSource ReportSource);
-
-    List<Report> findAllByReporterId(Long reporterId);
 
     @Modifying
     @Query("UPDATE Report r SET r.reporter = null WHERE r.reporter = :member")
