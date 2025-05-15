7cd643f2a4a60b4bc54efb4bd57003a238026472
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2025-05-15 21:51:49 +0900

    [COZY-608] feat : 메일 인증 리팩토링

--- src/main/java/com/cozymate/cozymate_server/domain/mail/repository/MailAuthenticationRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/mail/repository/MailAuthenticationRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MailRepository extends JpaRepository<MailAuthentication, String> {
+public interface MailAuthenticationRepository extends JpaRepository<MailAuthentication, String> {
     Optional<MailAuthentication> findByMailAddress(String mailAddress);
 
     List<MailAuthentication> findAllByMailAddress(String mailAddress);
