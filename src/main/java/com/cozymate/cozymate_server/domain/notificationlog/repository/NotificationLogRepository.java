70e642a99058b2bc300da407346e6ff68af23e9f
momuzzi <151748893+momuzzi@users.noreply.github.com>
2025-07-04 17:28:56 +0900

    [COZY-642] feat: 탈퇴한 사용자 / 삭제된 방에 대해 NotificationLog의 targetId를 null로 수정한다 (#356)

--- src/main/java/com/cozymate/cozymate_server/domain/notificationlog/repository/NotificationLogRepository.java
+++ src/main/java/com/cozymate/cozymate_server/domain/notificationlog/repository/NotificationLogRepository.java
@@ -18,4 +18,31 @@ public interface NotificationLogRepository extends JpaRepository<NotificationLog
     @Modifying
     @Query("delete from NotificationLog n where n.member.id = :memberId")
     void deleteAllByMemberId(@Param("memberId") Long memberId);
+
+    @Modifying
+    @Query("""
+        update NotificationLog nl
+        set nl.targetId = null
+        where nl.targetId = :memberId
+        and nl.category in :categoryList
+        and (nl.content not like concat('%', :pattern1) and nl.content not like concat('%', :pattern2))
+    """
+    )
+    void updateTargetIdToNullByMemberId(
+        @Param("memberId") Long memberId, @Param("categoryList") List<String> categoryList,
+        @Param("pattern1") String pattern1, @Param("pattern2") String pattern2
+    );
+
+    @Modifying
+    @Query("""
+        update NotificationLog nl
+        set nl.targetId = null
+        where nl.targetId = :roomId
+        and (nl.category = 'ROOM' or (nl.category in :categoryList and (nl.content like concat('%', :pattern1) or nl.content like concat('%', :pattern2))))
+    """
+    )
+    void updateTargetIdToNullByRoomId(
+        @Param("roomId") Long roomId, @Param("categoryList") List<String> categoryList,
+        @Param("pattern1") String pattern1, @Param("pattern2") String pattern2
+    );
 }
