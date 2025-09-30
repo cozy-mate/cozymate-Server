71fe8975cc8ae367e1a7102e356a1357e1c868d6
suuu0719 <suuu0719@gmail.com>
2025-09-30 20:43:55 +0900

    [COZY-663] feat: S3Presigner Bean 등록

--- src/main/java/com/cozymate/cozymate_server/global/s3/config/S3Config.java
+++ src/main/java/com/cozymate/cozymate_server/global/s3/config/S3Config.java
@@ -1,32 +1,44 @@
 package com.cozymate.cozymate_server.global.s3.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.accessKey}")
+    @Value("${spring.cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secretKey}")
+    @Value("${spring.cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${spring.cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    public S3Client client() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
 
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-            .withRegion(region)
+        return S3Client.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+            .region(Region.of(region))
             .build();
     }
+
+    @Bean
+    public S3Presigner presigner() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Presigner.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+            .region(Region.of(region))
+            .build();
+    }
+
 }
