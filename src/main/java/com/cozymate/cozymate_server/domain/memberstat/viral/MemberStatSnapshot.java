371899f2e25370c6796af940b198ef6374817427
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2025-10-05 20:50:41 +0900

    [COZY-667] fix: 불필요한 PrePersist 삭제

--- src/main/java/com/cozymate/cozymate_server/domain/memberstat/viral/MemberStatSnapshot.java
+++ src/main/java/com/cozymate/cozymate_server/domain/memberstat/viral/MemberStatSnapshot.java
@@ -32,7 +32,7 @@ import lombok.NoArgsConstructor;
 public class MemberStatSnapshot extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mss_seq_gen")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
@@ -45,42 +45,5 @@ public class MemberStatSnapshot extends BaseTimeEntity {
     @Builder(toBuilder = true)
     public MemberStatSnapshot(Lifestyle lifestyle) {
         this.lifestyle = lifestyle;
-    }
-
-    @PrePersist
-    void onPrePersist() {
-        if (viralCode == null) {
-            long mixed = mix(id); // 랜덤함을 보이기 위한 섞기.
-            this.viralCode = toBase57Fixed(mixed, 6);
-        }
-    }
-
-    private static final char[] ALPHABET = (
-        "ABCDEFGHJKMNPQRSTUVWXYZ" +
-            "abcdefghijkmnpqrstuvwxyz" +
-            "123456789"
-    ).toCharArray();
-    private static final int RADIX = ALPHABET.length;
-
-    private static String toBase57Fixed(long n, int width) {
-        StringBuilder sb = new StringBuilder();
-        do {
-            long rem = Long.remainderUnsigned(n, RADIX);
-            sb.append(ALPHABET[(int) rem]);
-            n = Long.divideUnsigned(n, RADIX);
-        } while (Long.compareUnsigned(n, 0L) > 0);
-        while (sb.length() < width) {
-            sb.append(ALPHABET[0]);
-        }
-        return sb.reverse().toString();
-    }
-
-    private static long mix(long x) {
-        x ^= 0x5A17_2025CAFEL;
-        x *= 0x9E3779B97F4A7C15L;
-        x ^= (x >>> 33);
-        x *= 0xC2B2AE3D27D4EB4FL;
-        x ^= (x >>> 29);
-        return x;
     }
 }
