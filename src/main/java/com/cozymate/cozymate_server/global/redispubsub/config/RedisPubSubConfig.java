ada70ce79f2541268eca30226a42317661afd5ed
veronees <jsg6854@naver.com>
2025-10-15 22:25:44 +0900

    [COZY-665] feat : Redis Listener 스레드 풀 설정 추가

--- src/main/java/com/cozymate/cozymate_server/global/redispubsub/config/RedisPubSubConfig.java
+++ src/main/java/com/cozymate/cozymate_server/global/redispubsub/config/RedisPubSubConfig.java
@@ -1,12 +1,15 @@
 package com.cozymate.cozymate_server.global.redispubsub.config;
 
 import com.cozymate.cozymate_server.global.redispubsub.RedisPubSubListener;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,6 +27,23 @@ public class RedisPubSubConfig {
     public RedisMessageListenerContainer redisMessageListenerContainer() {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(redisConnectionFactory);
+        container.setTaskExecutor(redisListenerExecutor());
         return container;
+    }
+
+    @Bean
+    public Executor redisListenerExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(200);
+        executor.setRejectedExecutionHandler(new CallerRunsPolicy());
+        executor.setKeepAliveSeconds(20);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(10);
+        executor.setThreadNamePrefix("redis-listener-");
+        executor.initialize();
+
+        return executor;
     }
 }
