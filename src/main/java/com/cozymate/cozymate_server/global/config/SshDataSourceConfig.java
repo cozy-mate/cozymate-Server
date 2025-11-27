cfa0cf82da24458d2a21320846d24628a0addc51
Ga Dong Sik <ret0422@gmail.com>
2025-11-28 01:36:09 +0900

    [COZY-586] SSH 터널 설정 및 Redis DB 설정 변경
    SSH 터널 연결 설정을 변경하고, Redis DB 설정을 추가합니다.
    
    application 설정 파일 위치를 .gitignore에 추가하여 관리합니다.
    
    설정 값들을 application.yml 에서 읽어오도록 변경하여 관리 편의성을 높입니다.
    
    Redis 연결 시 database 정보를 포함하도록 수정하여, 분리된 환경을 지원합니다.
    
    [COZY-586]

--- src/main/java/com/cozymate/cozymate_server/global/config/SshDataSourceConfig.java
+++ src/main/java/com/cozymate/cozymate_server/global/config/SshDataSourceConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import software.amazon.awssdk.services.s3.endpoints.internal.Value.Bool;
 
 @Slf4j
 @Configuration
@@ -21,32 +22,38 @@ public class SshDataSourceConfig {
     private final SshTunnelConfig initializer;
 
     @Value("${server}")
-    private String isServer;
+    private boolean noSshTunneling;
 
-    @Value("${spring.cloud.aws.ec2.database_endpoint}")
+    @Value("${ssh_tunnel.database_tunnel_endpoint}")
+    private String databaseTunnelEndppoint;
+    @Value("${ssh_tunnel.database_endpoint}")
     private String databaseEndpoint;
-    @Value("${spring.cloud.aws.ec2.database_port}")
+    @Value("${ssh_tunnel.database_port}")
     private int databasePort;
 
 
     @Bean("dataSource")
     @Primary
     public DataSource dataSource(DataSourceProperties properties) {
-        String url = properties.getUrl().replace("localhost", databaseEndpoint);
-        url = url.replace("[forwardedPort]", String.valueOf(3306));
-        Integer forwardedPort = null;
+        String url = properties.getUrl(); // jdbc:p6spy:mysql://[host]:[port]/...
 
         // SSH 터널을 통해 RDS에 연결해야 할 경우
-        if (isServer.equals("false")) {
-            url = properties.getUrl(); // jdbc:mysql://localhost:[forwardedPort]/dev
-            forwardedPort = initializer.buildSshConnection(databaseEndpoint, databasePort);
-            url = url.replace("[forwardedPort]", String.valueOf(forwardedPort));
+        if (!noSshTunneling) {
+            // 터널링 사용하는 경우: 로컬 포트로 바꿔 끼우기
+            int forwardedPort = initializer.buildSshConnection(databaseTunnelEndppoint, databasePort);
+            url = url
+                .replace("[host]", databaseTunnelEndppoint)
+                .replace("[port]", String.valueOf(forwardedPort));
+        } else {
+            // 터널 없이 infra-mysql 로 직접 붙는 경우
+            url = url
+                .replace("[host]", databaseEndpoint)
+                .replace("[port]", String.valueOf(databasePort));
         }
 
         log.info("Datasource Properties URL: {}", url);
         log.info("Datasource Properties Username: {}", properties.getUsername());
         log.info("Datasource Properties Driver ClassName: {}", properties.getDriverClassName());
-
         log.info(url);
         return DataSourceBuilder.create()
             .url(url)
